32e8c3951662d8825bb11b941c2c6fc91dc68162
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-06-04 23:59:47 +0530

    Create Sort_0_1_2.cpp

---
+++ Day 1/Sort_0_1_2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+    int count0 = 0;
+    int count1 = 0;
+    int count2 = 0;
+
+    for(int i = 0; i < n; i++) {
+
+      if(arr[i] == 0) {
+        count0++;
+      }
+      else if(arr[i] == 1) {
+        count1++;
+      }
+      else{
+        count2++;
+      }
+    }
+
+    for(int i = 0; i < count0; i++) {
+      arr[i] = 0;
+    }
+    for(int i = count0; i < count0+count1; i++) {
+      arr[i] = 1;
+    }
+    for(int i = count0+count1; i < n; i++) {
+      arr[i] = 2;
+    }
+}
