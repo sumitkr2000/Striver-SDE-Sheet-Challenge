7d929998e59d9444b84f7a658f256db48f7a3be1
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-06-10 21:00:30 +0530

    Create Print Permutations - String.cpp

---
+++ Day 6/Print Permutations - String.cpp
@@ -0,0 +1,23 @@
+void findPermutationsfun(string &s, int i, vector<string>& ans) {
+
+      //base case
+      if(i >= s.length()) {
+          ans.push_back(s);
+          return;
+      }
+
+      for(int j = i; j<s.length(); j++) {
+          swap(s[i], s[j]);
+          findPermutationsfun(s, i+1, ans);
+          //backtrack
+          swap(s[i], s[j]);
+      }
+  }
+
+vector<string> findPermutations(string &s) {
+
+  vector<string> ans;
+    int i = 0;
+    findPermutationsfun(s, i, ans);
+    return ans;
+ }
