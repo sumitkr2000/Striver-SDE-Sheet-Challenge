d1884324e610cb87e700f7fc1c059f27cf06919d
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-07-17 19:45:46 +0530

    Update Balanced Binary Tree.cpp

--- Day 26/Balanced Binary Tree.cpp
+++ Day 26/Balanced Binary Tree.cpp
@@ -1,33 +1,30 @@
-class Solution {
-private:
-    pair<bool,int> solve(TreeNode* root) {
-        //base case
-        if(root == NULL) {
-            pair<bool,int> p = make_pair(true,0);
-            return p;
+long countWaysToMakeChange(int *denominations, int n, int value)
+{
+    long dp[n+1][value+1];
+        
+    for(int i = 0; i < n+1; i++) {
+        for(int j = 0; j < value+1; j++) {
+
+            if(i == 0) {
+                dp[i][j] = 0;
+            }
+            if(j == 0) {
+                dp[i][j] = 1;
+            }
         }
-        
-        pair<bool,int> left = solve(root -> left);
-        pair<bool,int> right = solve(root -> right);
-        
-        bool leftAns = left.first;
-        bool rightAns = right.first;
-        bool diff = abs(left.second - right.second) <= 1;
-        
-        pair<bool,int> ans;
-        ans.second = max(left.second, right.second) + 1;
-        
-        if(leftAns && rightAns && diff) {
-            ans.first = true;
-        }
-        else {
-            ans.first = false;
-        }
-        return ans;
     }
-public:
-    bool isBalanced(TreeNode* root) {
-        
-        return solve(root).first;
+
+    for(int i = 1; i < n+1; i++) {
+        for(int j = 1; j < value+1; j++) {
+
+            if(denominations[i-1] <= j) {
+                dp[i][j] = dp[i][j-denominations[i-1]] + dp[i-1][j];
+            }
+            else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
     }
-};
+
+    return dp[n][value];
+}
