beb1818a71cb97ca71878c71d391aed14a79d5f5
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-07-17 15:03:37 +0530

    Create Best Time to Buy and Sell Stock.cpp

---
+++ Day 1/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    
+    int ans = 0;
+    int mini = INT_MAX;
+        
+    for(int i = 0; i < prices.size(); i++) {
+        mini = min(mini, prices[i]);
+        ans = max(ans, prices[i]-mini);
+    }
+    return ans;
+}
