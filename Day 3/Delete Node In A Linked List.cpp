4e8ec0631ab9bcb806aaf330f101bb602f3b77c2
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-07-17 15:10:43 +0530

    Update and rename Delete Node In A Linked List.cpp to Majority element.cpp

--- Day 3/Delete Node In A Linked List.cpp
+++
@@ -1,4 +0,0 @@
-void deleteNode(LinkedListNode<int> * node) {
-  node -> data = node -> next -> data;
-  node -> next = node -> next -> next;
-} 
---
+++ Day 3/Majority element.cpp
@@ -0,0 +1,16 @@
+#include<map>
+int findMajorityElement(int arr[], int n) {
+    map<int, int> mp;
+
+    for(int i = 0; i < n; i++) {
+        mp[arr[i]]++;
+    }
+
+    for(auto i: mp) {
+        if(i.second > n/2) {
+            return i.first;
+            break;
+        }
+    }
+    return -1;
+}
