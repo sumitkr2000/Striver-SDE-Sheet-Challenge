d7240b893504e426ac4a87569b7c6e21536ad933
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-09-06 15:23:13 +0530

    Create Combination Sum.cpp

---
+++ Day 9/Combination Sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    void solve(int i, int target, vector<int> &candidates, vector<int> &op, vector<vector<int>> &ans) {
+        //base case
+        if(i == candidates.size()) {
+            if(target == 0) {
+                ans.push_back(op);
+            }
+            return;
+        }
+        
+        solve(i+1, target, candidates, op, ans);
+        
+        if(candidates[i] <= target) {
+            op.push_back(candidates[i]);
+            solve(i, target-candidates[i], candidates, op, ans);
+            op.pop_back();
+        }
+    }
+    
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        vector<vector<int>> ans;
+        vector<int> op;
+        
+        solve(0, target, candidates, op, ans);
+        return ans;
+    }
+};
