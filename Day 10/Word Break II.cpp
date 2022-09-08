c7604417bc9eaf76b86cd9ef0354f8a54f89e04e
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-09-08 09:18:26 +0530

    Create Word Break II.cpp

---
+++ Day 10/Word Break II.cpp
@@ -0,0 +1,36 @@
+void solve(int i, string &s, unordered_map<string, bool> &mp, vector<string> &op, vector<string> &ans) {
+    //base case
+    if(i == s.size()) {
+        string res = "";
+        for(auto it: op) {
+            res += it;
+            res += ' ';
+        }
+        res.pop_back();
+        ans.push_back(res);
+        return;
+    }
+    
+    for(int j = i; j < s.size(); j++) {
+        string word = s.substr(i, j-i+1);
+        if(mp[word] == true) {
+            op.push_back(word);
+            solve(j + 1, s, mp, op, ans);
+            op.pop_back();
+        }
+    }
+}
+
+vector<string> wordBreak(string &s, vector<string> &dictionary)
+{
+    unordered_map<string, bool> mp;
+    for(auto it: dictionary) {
+        mp[it] = true;
+    }
+    
+    vector<string> ans;
+    vector<string> op;
+    
+    solve(0, s, mp, op, ans);
+    return ans;
+}
