d3f728b9ee07e0a4e2e52bcffbb40d1fe9a61221
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-09-10 14:25:40 +0530

    Update Stack using queue.cpp

--- Day 13/Stack using queue.cpp
+++ Day 13/Stack using queue.cpp
@@ -1,69 +1,29 @@
-#include<queue>
-class Stack {
-	queue<int> q;
-    queue<int> p;
-
-   public:
-    Stack() {
-        // Implement the Constructor.
+class MyStack {
+public:
+    queue<int> q;
+    
+    MyStack() {       
     }
-
-    /*----------------- Public Functions of Stack -----------------*/
-
-    int getSize() {
-       return q.size();
-    }
-
-    bool isEmpty() {
-        return q.empty();
-    }
-
-    void push(int element) {
-        q.push(element);        
-    }
-
-    int pop() {
-        
-        if(q.empty()) {
-            return -1;
-        }
-        else{
-           while(q.size() != 1) {  
-                p.push(q.front());
-                q.pop();
-            }
-
-            int y = q.front();
+    
+    void push(int x) { 
+        q.push(x);
+        for(int i = 0 ; i < q.size()-1; i++) {
+            q.push(q.front());
             q.pop();
-
-            while(p.size() != 0){
-                q.push(p.front()); 
-                p.pop();
-            }
-            return y; 
-        }   
+        }
     }
-        
-
+    
+    int pop() {
+        int top = q.front();
+        q.pop();
+        return top;
+    }
+    
     int top() {
-        
-        if(q.empty()) {
-            return -1;
-        }
-        else {
-           while(q.size() != 1) {
-               p.push(q.front());
-               q.pop();
-           }
-            
-           int y = q.front(); 
-           p.push(q.front());
-           q.pop();
-           while(!p.empty()){
-               q.push(p.front());
-               p.pop();
-           }
-           return y; 
-        }
+        return q.front();
+    }
+    
+    bool empty() { 
+        return q.size() == 0;
     }
 };
