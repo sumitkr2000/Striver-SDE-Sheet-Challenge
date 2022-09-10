7618067fe82117cd727883651902ecbc922a9bdb
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-09-10 14:25:12 +0530

    Create Implement Queue using Stacks.cpp

---
+++ Day 13/Implement Queue using Stacks.cpp
@@ -0,0 +1,38 @@
+class MyQueue {
+public:
+    stack<int> st1;
+    stack<int> st2;
+    
+    MyQueue() {    
+    }
+    
+    void push(int x) {
+        st1.push(x);
+    }
+    
+    int pop() {
+        if(st2.empty()) {
+            while(!st1.empty()) {
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+        int front = st2.top();
+        st2.pop();
+        return front;
+    }
+    
+    int peek() {
+        if(st2.empty()) {
+            while(!st1.empty()) {
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+        return st2.top();
+    }
+    
+    bool empty() {
+        return (st1.size() | st2.size()) == 0;
+    }
+};
