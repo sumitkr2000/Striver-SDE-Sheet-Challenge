e67aed0b64f5c7f5f2df9e5eddf7075ffba1af97
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-09-10 14:21:42 +0530

    Create Maximum Sum Combinations.cpp

---
+++ Day 12/Maximum Sum Combinations.cpp
@@ -0,0 +1,34 @@
+vector<int> Solution::solve(vector<int> &A, vector<int> &B, int C) {
+    
+    int n = A.size();
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    
+    priority_queue<pair<int, pair<int, int>>> pq;
+    pq.push({A[n-1]+B[n-1], {n-1, n-1}});
+    
+    set<pair<int, int>> st;
+    st.insert({n-1, n-1});
+    
+    vector<int> ans;
+    
+    while(C > 0) {
+        pair<int, pair<int, int>> top = pq.top();
+        pq.pop();
+        ans.push_back(top.first);
+        
+        int i = top.second.first;
+        int j = top.second.second;
+        
+        if(i > 0 && st.find({i-1, j}) == st.end()) {
+            pq.push({A[i-1]+B[j], {i-1, j}});
+            st.insert({i-1, j});
+        }
+        if(j > 0 && st.find({i, j-1}) == st.end()) {
+            pq.push({A[i]+B[j-1], {i, j-1}});
+            st.insert({i, j-1});
+        }
+        C--;
+    }
+    return ans;
+}
