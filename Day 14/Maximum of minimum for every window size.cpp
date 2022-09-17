accd330cad9308aff1216b86fed34c853e06b1c1
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-09-17 13:04:56 +0530

    Create Maximum of minimum for every window size.cpp

---
+++ Day 14/Maximum of minimum for every window size.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h> 
+vector<int> maxMinWindow(vector<int> a, int n) {
+    
+    vector<int> nse(n, n);
+    stack<int> st;
+
+    for(int i = 0; i < n; i++) {
+        if(!st.empty() && a[i] < a[st.top()]) {
+            while(!st.empty() && a[i] < a[st.top()]) {
+                nse[st.top()] = i;
+                st.pop();
+            }
+        }
+        st.push(i);
+    }
+
+    while(!st.empty()) {
+        st.pop();
+    }
+    vector<int> pse(n, -1);
+
+    for(int i = n-1; i >= 0; i--) {
+        if(!st.empty() && a[i] < a[st.top()]) {
+            while(!st.empty() && a[i] < a[st.top()]) {
+                pse[st.top()] = i;
+                st.pop();
+            }
+        }
+        st.push(i);
+    }
+
+    vector<int> ans(n, INT_MIN);
+
+    for(int i = 0; i < n; i++) {
+        int ind = nse[i] - pse[i] - 2;
+        ans[ind] = max(ans[ind], a[i]);
+    }
+
+    for(int i = n-2; i >= 0; i--) {
+        ans[i] = max(ans[i], ans[i+1]);
+    }
+
+    return ans;
+}
