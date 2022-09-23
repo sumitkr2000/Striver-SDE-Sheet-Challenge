0d8534c952c9a5b727f91aff1d5d94b5fb811827
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-09-23 19:38:29 +0530

    Update Tree Traversals.cpp

--- Day 17/Tree Traversals.cpp
+++ Day 17/Tree Traversals.cpp
@@ -1,29 +1,39 @@
-void singleTraversal(BinaryTreeNode<int> *root, vector<int> &in, vector<int> &pre, vector<int> &post) {
-
-    if(root == NULL) {
-        return;
-    }
-
-    pre.push_back(root -> data);
-    singleTraversal(root -> left, in, pre, post);
-    in.push_back(root -> data);
-    singleTraversal(root -> right, in, pre, post);
-    post.push_back(root -> data);
-}
-
+#include<stack>
 vector<vector<int>> getTreeTraversal(BinaryTreeNode<int> *root){
     
-    vector<int> in;    
-    vector<int> pre;    
-    vector<int> post;
-    
-    singleTraversal(root, in, pre, post);
-    
+    vector<int> in, pre, post;
     vector<vector<int>> ans;
+    stack<pair<BinaryTreeNode<int>*, int>> st;
+    st.push({root, 1});
     
+    if(root == NULL) {
+        return ans;
+    }
+    
+    while(!st.empty()) {
+        if(st.top().second == 1) {
+            pre.push_back(st.top().first -> data);
+            st.top().second++;
+            
+            if(st.top().first -> left) {
+                st.push({st.top().first -> left, 1});
+            }
+        }
+        else if(st.top().second == 2) {
+            in.push_back(st.top().first -> data);
+            st.top().second++;
+            
+            if(st.top().first -> right) {
+                st.push({st.top().first -> right, 1});
+            }
+        }
+        else {
+            post.push_back(st.top().first -> data);
+            st.pop();
+        }
+    }
     ans.push_back(in);
     ans.push_back(pre);
     ans.push_back(post);
-   
     return ans;
 }
