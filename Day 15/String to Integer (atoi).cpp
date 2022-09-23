9d83e85bd6e6ef7d9bbc727008d789b652450b9b
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-09-23 19:28:12 +0530

    Create String to Integer (atoi).cpp

---
+++ Day 15/String to Integer (atoi).cpp
@@ -0,0 +1,48 @@
+class Solution {
+private:
+    bool isDigit(char digit) {
+        return digit >= '0' && digit <= '9';
+    }
+    
+public:
+    int myAtoi(string s) {
+        
+        int n = s.size();
+        int i = 0;
+        int sign = 1;
+        int ans = 0;
+        
+        while(i < n && s[i] == ' ') i++;
+        
+        if(s[i] == '+') {
+            i++;
+        }
+        else if(s[i] == '-') {
+            sign = -1;
+            i++;
+        }
+        
+        while(i < n && isDigit(s[i])) {
+            
+            int digit = s[i] - '0';
+            
+            if(ans > (INT_MAX - digit) / 10) {
+                // cout<<"came";
+                if(sign == 1) {
+                    return INT_MAX;
+                }
+                else {
+                    return INT_MIN;
+                }
+            }
+            
+            ans = ans * 10 + digit;
+            i++;
+        }
+        if(ans != 0 && sign == -1) {
+            return -ans;
+        }
+        
+        return ans;
+    }
+};
