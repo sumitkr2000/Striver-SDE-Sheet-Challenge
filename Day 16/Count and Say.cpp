15af219e41021c7dc82bb21dcf59e40718e49df3
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-09-23 19:32:29 +0530

    Create Count and Say.cpp

---
+++ Day 16/Count and Say.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        
+        if(n == 1) {
+            return "1";
+        }
+        
+        string ans = "1";
+        
+        for(int i = 2; i <= n; i++) {
+            string temp;
+            int cnt = 1;
+            for(int j = 0; j < ans.size(); j++) {
+                if(j == ans.size()-1 || ans[j] != ans[j+1]) {
+                    temp += to_string(cnt);
+                    temp += ans[j];
+                    cnt = 0;
+                }
+                cnt++;
+            }
+            ans = temp;
+        }
+        return ans;
+    }
+};
