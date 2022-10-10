9221b35902e150876055e9fc4c05543b2e811a4c
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-10-11 00:12:36 +0530

    Create Children Sum Property.cpp

---
+++ Day 19/Children Sum Property.cpp
@@ -0,0 +1,40 @@
+void changeTree(BinaryTreeNode<int>* root) {
+    
+    if(root == NULL) {
+        return;
+    }
+    
+    int childSum = 0;
+    if(root -> left) {
+        childSum += root -> left -> data;
+    }
+    if(root -> right) {
+        childSum += root -> right -> data;
+    }
+    
+    if(root -> data <= childSum) {
+        root -> data = childSum;
+    }
+    else {
+        if(root -> left) {
+            root -> left -> data = root -> data;
+        }
+        if(root -> right) {
+            root -> right -> data = root -> data;
+        }
+    }
+    
+    changeTree(root -> left);
+    changeTree(root -> right);
+    
+    int sum = 0;
+    if(root -> left) {
+        sum += root -> left -> data;
+    }
+    if(root -> right) {
+        sum += root -> right -> data;
+    }
+    if(root -> left || root -> right) {
+        root -> data = sum;
+    }
+}
