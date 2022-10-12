e435f8032566b3fdc392a9c984e2c4b6cfdfb11e
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-10-12 11:08:07 +0530

    Create Topological Sort DFS.cpp

---
+++ Topological Sort DFS.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+    private:
+    void topoDFS(int node, vector<int> adj[], unordered_map<int, bool> &vis, stack<int> &st) {
+        
+        vis[node] = 1;
+        
+        for(auto it: adj[node]) {
+            if(!vis[it]) {
+                topoDFS(it, adj, vis, st);
+            }
+        }
+        st.push(node);
+    }
+    
+	public:
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    DFS
+	   unordered_map<int, bool> vis;
+	   stack<int> st;
+	   vector<int> ans;
+	    
+	   for(int i = 0; i < V; i++) {
+	       if(!vis[i]) {
+	           topoDFS(i, adj, vis, st);
+	       }
+	   }
+	    
+	   while(!st.empty()) {
+	       ans.push_back(st.top());
+	       st.pop();
+	   }
+	   return ans;
+	}
+};
