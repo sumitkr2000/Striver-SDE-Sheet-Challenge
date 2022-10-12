a90193d4d0c78a44f4945a9af0dd95ebf2db136f
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-10-12 11:07:02 +0530

    Create Topological Sort BFS.cpp

---
+++ Day 23/Topological Sort BFS.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+	public:
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int> indegree(V, 0);
+        for(int i = 0; i < V; i++) {
+            for(auto j: adj[i]) {
+                indegree[j]++;
+            }
+        }
+        
+        queue<int> q;
+        for(int i = 0; i < V; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        
+        vector<int> ans;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto it: adj[node]) {
+                indegree[it]--;
+                if(indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        return ans;
+	}
+};
