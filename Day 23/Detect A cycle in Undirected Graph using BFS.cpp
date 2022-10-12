4df8e0b86eabae8b27f10ead96bde94fe5b92c60
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-10-12 11:02:30 +0530

    Create Detect A cycle in Undirected Graph using BFS.cpp

---
+++ Day 23/Detect A cycle in Undirected Graph using BFS.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+bool bfsCycleDetection(int node, unordered_map<int, list<int>> &adjList, unordered_map<int, bool> &visited) {
+    
+    unordered_map<int, int> parent;
+    
+    queue<int> q;
+    q.push(node);
+    visited[node] = 1;
+    parent[node] = -1;
+    
+    while(!q.empty()) {
+        int front = q.front();
+        q.pop();
+        
+        for(auto it: adjList[front]) {
+            if(visited[it] && it != parent[front]) {
+                return true;
+            }            
+            else if(!visited[it]){
+                q.push(it);
+                visited[it] = 1;
+                parent[it] = front; 
+            }
+        }
+    }
+    return false;
+}
+
+string cycleDetection (vector<vector<int>>& edges, int n, int m)
+{
+    unordered_map<int, list<int>> adjList;    
+    for(int i = 0; i < m; i++) {        
+        int u = edges[i][0];
+        int v = edges[i][1];
+        
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+    
+    unordered_map<int, bool> visited;    
+    for(int i = 1; i <= n; i++) {
+        if(!visited[i]) {
+            bool ans = bfsCycleDetection(i, adjList, visited);
+            if(ans == true) {
+                return "Yes";
+            }
+        }
+    }
+    return "No";
+}
