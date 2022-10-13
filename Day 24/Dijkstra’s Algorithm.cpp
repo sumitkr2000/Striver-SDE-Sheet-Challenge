75ac30fef4de2c8159807d61966845b927d7c32c
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-10-13 14:03:20 +0530

    Create Dijkstra’s Algorithm.cpp

---
+++ Day 24/Dijkstra’s Algorithm.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> ans(V, INT_MAX);
+        pq.push({0, S});
+        ans[S] = 0;
+        
+        while(!pq.empty()) {
+            int node = pq.top().second;
+            int dist = pq.top().first;
+            pq.pop();
+            
+            for(auto it: adj[node]) {
+                int currNode = it[0];
+                int currDist = it[1];
+                
+                if(dist + currDist < ans[currNode]) {
+                    ans[currNode] = dist + currDist;
+                    pq.push({dist+currDist, currNode});
+                }
+            }
+        }
+        return ans;
+    }
+};
