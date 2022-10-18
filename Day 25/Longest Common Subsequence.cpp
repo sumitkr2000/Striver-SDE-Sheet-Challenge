5047f04c4ebdeff3f21b6fe8a74d879770fc5b50
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-10-18 17:53:07 +0530

    Update Longest Common Subsequence.cpp

--- Day 25/Longest Common Subsequence.cpp
+++ Day 25/Longest Common Subsequence.cpp
@@ -1,33 +1,65 @@
-int topDown(string s, string t, int n, int m) {
+class Solution {
+private:
+    int solve(int i, int j, string text1, string &text2,vector<vector<int>> &dp) {
         
-    int dp[n+1][m+1];
-
-    for(int i = 0; i < n+1; i++) {
-        for(int j = 0; j < m+1; j++) {
-            if(i == 0 || j == 0) {
-                dp[i][j] = 0;
+        if(i == text1.size() || j == text2.size()) {
+            return 0;
+        }
+        
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        
+        if(text1[i] == text2[j]) {
+            return dp[i][j] = 1 + solve(i+1, j+1, text1, text2, dp);
+        }
+        
+        return dp[i][j] = max(solve(i+1, j, text1, text2, dp), solve(i, j+1, text1, text2, dp));
+    }
+    
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        
+        int n = text1.size(), m = text2.size();
+        
+        //Approach 1: Memoization
+        vector<vector<int>> dp(n, vector<int>(m, -1));            
+        return solve(0, 0, text1, text2, dp);
+        
+        
+        /*//Approach 2: Tabulation
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = m-1; j >= 0; j--) {
+                if(text1[i] == text2[j]) {
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }
+                else {
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+                }
+                    
             }
         }
-    }
-
-
-    for(int i = 1; i < n+1; i++) {
-        for(int j = 1; j < m+1; j++) {
-            if(s[i-1] == t[j-1]) {
-                dp[i][j] = 1 + dp[i-1][j-1];
+        return dp[0][0];
+        */
+        
+        /*//Approach 3: Space optimized
+        vector<int> next(m+1, 0);
+        vector<int> curr(m+1, 0);
+        
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = m-1; j >= 0; j--) {
+                if(text1[i] == text2[j]) {
+                    curr[j] = 1 + next[j+1];
+                }
+                else {
+                    curr[j] = max(next[j], curr[j+1]);
+                }     
             }
-            else {
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-            }
+            next = curr;
         }
+        return next[0];
+        */
     }
-    return dp[n][m];        
-} 
-
-int lcs(string s, string t)
-{
-	int n = s.size();
-    int m = t.size();
-
-    return topDown(s, t, n, m);
-}
+};
