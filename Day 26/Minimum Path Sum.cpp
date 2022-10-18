2636753d49b87402d2648ebef5191aefc9a2da8c
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-10-18 17:58:07 +0530

    Update Minimum Path Sum.cpp

--- Day 26/Minimum Path Sum.cpp
+++ Day 26/Minimum Path Sum.cpp
@@ -1,82 +1,87 @@
-#include<bits/stdc++.h>
-//Approach 1: Memoization
-int solve(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &dp) {
-    //base case
-    if(i == 0 && j == 0) {
-        return grid[0][0];
+class Solution {
+private:
+    int solve(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp) {
+        
+        if(i >= grid.size() || j >= grid[0].size()) {
+            return 1e9;
+        }
+        
+        if(i == grid.size()-1 && j == grid[0].size()-1) {
+            return grid[i][j];
+        }
+        
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        
+        int down = grid[i][j] + solve(i+1, j, grid, dp);
+        int right = grid[i][j] + solve(i, j+1, grid, dp);
+        
+        return dp[i][j] = min(down ,right);
     }
-
-    if(i < 0 || j < 0) {
-        return 1e9;
-    }
-
-    if(dp[i][j] != -1) {
-        return dp[i][j];
-    }
-
-    int up = grid[i][j] + solve(grid, i-1, j, dp);
-    int left = grid[i][j] + solve(grid, i, j-1, dp);
-
-    return dp[i][j] = min(up, left);
-}
-
-int minSumPath(vector<vector<int>> &grid) {
     
-    int n = grid.size();
-    int m = grid[0].size();
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        
+        int n = grid.size(), m = grid[0].size();
+        
+        //Approach 1: Memoization
+        vector<vector<int>> dp(n, vector<int>(m, -1));        
+        return solve(0, 0, grid, dp);
     
-    /* //Approach 1: Memoization
-    vector<vector<int>> dp(n, vector<int>(m, -1));
-    return solve(grid, n-1, m-1, dp);
-    */
-    
-    /* //Approach 2: Tabulation
-    vector<vector<int>> dp(n, vector<int>(m));
-    
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if(i == 0 && j == 0) {
-                dp[i][j] = grid[i][j];
-            }
-            else {
-                int up = 1e9;
-                int left = 1e9;
-                if(i > 0) {
-                    up = grid[i][j] + dp[i-1][j];
+        
+        /*//Approach 2: Tabulation
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = m-1; j>= 0; j--) {
+                if(i == n-1 && j == m-1) {
+                    dp[i][j] = grid[i][j];
                 }
-                if(j > 0) {
-                    left = grid[i][j] + dp[i][j-1];
+                else {
+                    int down = 1e9;
+                    if(i < n-1) {
+                        down = grid[i][j] + dp[i+1][j];
+                    }
+                    
+                    int right = 1e9;
+                    if(j < m-1) {
+                        right = grid[i][j] + dp[i][j+1];
+                    }
+                    
+                    dp[i][j] = min(down ,right);
                 }
-                dp[i][j] = min(up, left);
-            }
-         }
-    }
-    return dp[n-1][m-1];
-    */
-    
-    //Approach 3: Space optimisation
-    vector<int> prev(m, 0);
-    
-    for(int i = 0; i < n; i++) {
-        vector<int> temp(m, 0);
-        for(int j = 0; j < m; j++) {
-            if(i == 0 && j == 0) {
-                temp[j] = grid[i][j];
-            }
-            else{
-                int up = 1e9;
-                int left = 1e9;
-                
-                if(i > 0) {
-                    up = grid[i][j] + prev[j];
-                }
-                if(j > 0) {
-                    left = grid[i][j] + temp[j-1];
-                }
-                temp[j] = min(up, left);
             }
         }
-        prev = temp;
+        return dp[0][0];
+        */
+        
+        /*//Approach 3: Space optimized
+        vector<int> next(m, 0);
+        vector<int> curr(m, 0);
+        
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = m-1; j>= 0; j--) {
+                if(i == n-1 && j == m-1) {
+                    curr[j] = grid[i][j];
+                }
+                else {
+                    int down = 1e9;
+                    if(i < n-1) {
+                        down = grid[i][j] + next[j];
+                    }
+                    
+                    int right = 1e9;
+                    if(j < m-1) {
+                        right = grid[i][j] + curr[j+1];
+                    }
+                    
+                    curr[j] = min(down ,right);
+                }
+            }
+            next = curr;
+        }
+        return next[0];
+        */
     }
-    return prev[m-1];
-}
+};
