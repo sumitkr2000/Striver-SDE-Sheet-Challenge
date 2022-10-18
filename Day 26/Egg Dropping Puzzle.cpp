296669b45b6e180a15d7c3d1d3ca1607e9a8b8b4
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-10-18 17:57:38 +0530

    Update Egg Dropping Puzzle.cpp

--- Day 26/Egg Dropping Puzzle.cpp
+++ Day 26/Egg Dropping Puzzle.cpp
@@ -2,8 +2,8 @@ class Solution
 {
     private:
     int solve(int n, int k, vector<vector<int>> &dp) {
-        //base cond.
-        if(n == 1 || k == 0 || k == 1) {
+        
+        if(k == 0 || k == 1 || n == 1) {
             return k;
         }
         
@@ -11,39 +11,19 @@ class Solution
             return dp[n][k];
         }
         
-        int ans = INT_MAX;
-                
+        int mini = INT_MAX;
         for(int i = 1; i <= k; i++) {
-            
-            int low;
-            if(dp[n-1][i-1] != -1) {
-                low = dp[n-1][i-1];
-            }
-            else {
-                low = solve(n-1, i-1, dp);
-                dp[n-1][i-1] = low;
-            }
-            
-            int high;
-            if(dp[n][k-i] != -1) {
-                high = dp[n][k-i];
-            }
-            else {
-                high = solve(n, k-i, dp);
-                dp[n][k-i] = high;
-            }
-            
-            int tempAns = 1 + max(low, high);
-            
-            ans = min(ans, tempAns);
+            int temp = 1 + max(solve(n-1, i-1, dp), solve(n, k-i, dp));
+            mini = min(mini, temp);
         }
-        return dp[n][k] = ans;
+        
+        return dp[n][k] = mini;
     }
     
     public:
     int eggDrop(int n, int k) 
     {
-        vector<vector<int>> dp(n+1, vector<int> (k+1 ,-1));
+        vector<vector<int>> dp(n+1, vector<int>(k+1, -1));
         return solve(n, k, dp);
     }
 };
