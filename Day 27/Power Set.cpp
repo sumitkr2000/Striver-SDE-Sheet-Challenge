d148bdaedeadfbc48553e3d439e18fbdcaec7efe
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-10-18 18:04:22 +0530

    Update Power Set.cpp

--- Day 27/Power Set.cpp
+++ Day 27/Power Set.cpp
@@ -1,32 +1,20 @@
-#include<bits/stdc++.h>
-void solve(vector<int> v, vector<int> &output, vector<vector<int>> &ans) {
-    //base case
-    if(v.size() == 0) {
-        if(output.size() != 0) {
-            ans.push_back(output);
-        }
-        return;
-    }
-
-    vector<int> op1 = output;
-    vector<int> op2 = output;
-
-    op2.push_back(v[0]);
-    v.erase(v.begin() + 0);
-
-    solve(v, op1, ans);
-    solve(v, op2, ans);
-
-    return;
-}
-
-vector<vector<int>> pwset(vector<int>v)
-{
-    vector<vector<int>> ans;
-    vector<int> output;
-
-    solve(v, output, ans);
-
-    sort(ans.begin(), ans.end());
-    return ans;
-}
+class Solution{
+public:
+	vector<string> AllPossibleStrings(string s){
+	    
+	    int n = s.size();
+	    vector<string> ans;
+	    
+	    for(int i = 1; i < (1 << n); i++) {
+	        string temp = "";
+	        for(int j = 0; j < n; j++) {
+	            if(i & (1 << j)) {
+	                temp += s[j];
+	            }
+	        }
+	        ans.push_back(temp);
+	    }
+	    sort(ans.begin(), ans.end());
+	    return ans;
+	}
+};
