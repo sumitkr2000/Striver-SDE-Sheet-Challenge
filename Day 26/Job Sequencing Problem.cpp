dcf01ff17a8214bbb8b4790f6095fa04bcc85809
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-10-18 18:03:20 +0530

    Create Job Sequencing Problem.cpp

---
+++ Day 26/Job Sequencing Problem.cpp
@@ -0,0 +1,35 @@
+class Solution 
+{
+    private:
+    bool static comp(Job a, Job b) {
+        return a.profit > b.profit;
+    }
+    
+    public:
+    //Function to find the maximum profit and the number of jobs done.
+    vector<int> JobScheduling(Job arr[], int n) 
+    { 
+        
+        sort(arr, arr+n, comp);
+        
+        int maxi = INT_MIN;
+        for(int i = 0; i < n; i++) {
+            maxi = max(maxi, arr[i].dead);
+        }
+        
+        vector<int> jobSeq(maxi+1, -1);
+        int job = 0, profit = 0;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = arr[i].dead; j > 0; j--) {
+                if(jobSeq[j] == -1) {
+                    jobSeq[j] = arr[i].id;
+                    job++;
+                    profit += arr[i].profit;
+                    break;
+                }
+            }
+        }
+        return {job, profit};
+    } 
+};
