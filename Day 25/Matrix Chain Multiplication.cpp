9a352426a838c31e24bbafd82ec59d1b6009ef4b
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-10-18 17:53:49 +0530

    Update Matrix Chain Multiplication.cpp

--- Day 25/Matrix Chain Multiplication.cpp
+++ Day 25/Matrix Chain Multiplication.cpp
@@ -1,27 +1,28 @@
-int solve(vector<int> &arr, int i, int j, vector<vector<int>> &dp) {
+class Solution{
+private:
+    int solve(int i, int j, int arr[], vector<vector<int>> &dp) {
         
-    if(i >= j) {
-        return 0;
-    }
-
-    if(dp[i][j] != -1) {
-        return dp[i][j];
-    }
-
-    int mini = INT_MAX;
-
-    for(int k = i; k < j; k++) {
-
-        int tempAns = solve(arr, i, k, dp) + solve(arr, k+1, j, dp) + arr[i-1]*arr[k]*arr[j];
-
-        mini = min(mini, tempAns);
-    }
-    return dp[i][j] = mini;
-}
-
-int matrixMultiplication(vector<int> &arr, int N)
-{
-    vector<vector<int>> dp(N+1, vector<int>(N+1, -1));
+        if(i == j) {
+            return 0;
+        }
         
-    return solve(arr, 1, N-1, dp);
-}
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        
+        int mini = INT_MAX;
+        for(int k = i; k < j; k++) {
+            int steps = arr[i-1]*arr[k]*arr[j] + solve(i, k, arr, dp) + solve(k+1, j, arr, dp);
+            mini = min(mini, steps);
+        }
+        
+        return dp[i][j] = mini;
+    }
+
+public:
+    int matrixMultiplication(int N, int arr[])
+    {
+        vector<vector<int>> dp(N, vector<int>(N, -1));
+        return solve(1, N-1, arr, dp);
+    }
+};
