4d77957e38019974ca8b26ba1916a59b159b23a8
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-11-05 15:45:05 +0530

    Create LRU Cache.cpp

---
+++ Day 14/LRU Cache.cpp
@@ -0,0 +1,73 @@
+class LRUCache {
+public:
+    class Node {
+    public:
+        int key;
+        int val;
+        Node* next;
+        Node* prev;
+        
+        Node(int k, int v) {
+            key = k;
+            val = v;
+            next = NULL;
+            prev = NULL;
+        }
+    };
+    
+    map<int, Node*> mp;
+    Node* head = new Node(-1, -1);
+    Node* tail = new Node(-1, -1);
+    int size;
+    
+    LRUCache(int capacity) {
+        head -> next = tail;
+        tail -> prev = head;
+        size = capacity;
+    }
+    
+    void addNode(Node* newNode) {
+        
+        Node* temp = head -> next;
+        newNode -> next = temp;
+        newNode -> prev = head;
+        head -> next = newNode;
+        temp -> prev = newNode;
+    }
+    
+    void deleteNode(Node* temp) {
+        
+        Node* nextNode = temp -> next;
+        Node* prevNode = temp -> prev;
+        prevNode -> next = nextNode;
+        nextNode -> prev = prevNode;
+    }
+    
+    int get(int key) {
+        if(mp.find(key) != mp.end()) {
+            Node* temp = mp[key];
+            int data = temp -> val;
+            mp.erase(key);
+            deleteNode(temp);
+            addNode(temp);
+            mp[key] = head -> next;
+            return data;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key) != mp.end()) {
+            Node* temp = mp[key];
+            mp.erase(key);
+            deleteNode(temp);
+        }
+        else if(mp.size() == size) {
+            mp.erase(tail -> prev -> key);
+            deleteNode(tail -> prev);
+        }
+        
+        addNode(new Node(key, value));
+        mp[key] = head -> next;
+    }
+};
