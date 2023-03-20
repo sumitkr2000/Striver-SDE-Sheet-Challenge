e8c177e4be4051a887c6dcd3befcd814d2233fac
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 12:24:48 +0530

    Update Maximum of minimum for every window size.cpp

--- Day 14/Maximum of minimum for every window size.cpp
+++ Day 14/Maximum of minimum for every window size.cpp
@@ -1,44 +1,43 @@
-#include <bits/stdc++.h> 
-vector<int> maxMinWindow(vector<int> a, int n) {
-    
-    vector<int> nse(n, n);
-    stack<int> st;
+class Solution {
+public:
+    vector<int> maxMinWindow(vector<int> a, int n) {
 
-    for(int i = 0; i < n; i++) {
-        if(!st.empty() && a[i] < a[st.top()]) {
+        vector<int> nse(n, n);
+        stack<int> st;
+
+        for(int i = 0; i < n; i++) {
             while(!st.empty() && a[i] < a[st.top()]) {
                 nse[st.top()] = i;
                 st.pop();
             }
+            st.push(i);
         }
-        st.push(i);
-    }
 
-    while(!st.empty()) {
-        st.pop();
-    }
-    vector<int> pse(n, -1);
+        while(!st.empty()) {
+            st.pop();
+        }
+        
+        vector<int> pse(n, -1);
 
-    for(int i = n-1; i >= 0; i--) {
-        if(!st.empty() && a[i] < a[st.top()]) {
+        for(int i = n-1; i >= 0; i--) {
             while(!st.empty() && a[i] < a[st.top()]) {
                 pse[st.top()] = i;
                 st.pop();
             }
+            st.push(i);
         }
-        st.push(i);
+
+        vector<int> ans(n, INT_MIN);
+
+        for(int i = 0; i < n; i++) {
+            int ind = nse[i] - pse[i] - 2;
+            ans[ind] = max(ans[ind], a[i]);
+        }
+
+        for(int i = n-2; i >= 0; i--) {
+            ans[i] = max(ans[i], ans[i+1]);
+        }
+
+        return ans;
     }
-
-    vector<int> ans(n, INT_MIN);
-
-    for(int i = 0; i < n; i++) {
-        int ind = nse[i] - pse[i] - 2;
-        ans[ind] = max(ans[ind], a[i]);
-    }
-
-    for(int i = n-2; i >= 0; i--) {
-        ans[i] = max(ans[i], ans[i+1]);
-    }
-
-    return ans;
-}
+};
