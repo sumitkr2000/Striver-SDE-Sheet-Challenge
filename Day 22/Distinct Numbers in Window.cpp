2cd9f4ee4a5c747531f0b6bbeb9134c9d7983b4b
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 14:02:16 +0530

    Update Distinct Numbers in Window.cpp

--- Day 22/Distinct Numbers in Window.cpp
+++ Day 22/Distinct Numbers in Window.cpp
@@ -1,24 +1,27 @@
-vector<int> Solution::dNums(vector<int> &A, int B) {
-    
-    vector<int> ans;
-    unordered_map<int, int> mp;
-    int i = 0, j = 0;
-    
-    while(i <= A.size()-B && j < A.size()) {
-        mp[A[j]]++;
-        
-        if(j-i >= B) {
-            mp[A[i]]--;
-            if(mp[A[i]] == 0) {
-                mp.erase(A[i]);
+class Solution {
+public:
+    vector<int> dNums(vector<int> &A, int B) {
+
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        int i = 0, j = 0;
+
+        while(i <= A.size()-B && j < A.size()) {
+            mp[A[j]]++;
+
+            if(j-i >= B) {
+                mp[A[i]]--;
+                if(mp[A[i]] == 0) {
+                    mp.erase(A[i]);
+                }
+                i++;
             }
-            i++;
+
+            if(j >= B-1) {
+               ans.push_back(mp.size());
+            }
+            j++;
         }
-        
-        if(j >= B-1) {
-           ans.push_back(mp.size());
-        }
-        j++;
+        return ans;
     }
-    return ans;
-}
+};
