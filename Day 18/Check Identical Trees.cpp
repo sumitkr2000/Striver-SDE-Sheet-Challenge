c2bb20b9c20c5ebe1582dea7d70d33db91d2630a
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:30:10 +0530

    Update Check Identical Trees.cpp

--- Day 18/Check Identical Trees.cpp
+++ Day 18/Check Identical Trees.cpp
@@ -1,24 +1,18 @@
-bool identicalTrees(BinaryTreeNode<int>* root1, BinaryTreeNode<int>* root2) {
-   
-    //base case
-    if(root1 == NULL && root2 == NULL) {
-        return true;
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(p == NULL && q == NULL) {
+            return true;
+        }
+        
+        if(p == NULL || q == NULL || p -> val != q -> val) {
+            return false;
+        }
+        
+        bool left = isSameTree(p -> left, q -> left);
+        bool right = isSameTree(p -> right, q -> right);
+        
+        return left & right;
     }
-    if(root1 == NULL && root2 != NULL) {
-        return false;
-    }
-    if(root1 != NULL && root2 == NULL) {
-        return false;
-    }
-
-    bool left = identicalTrees(root1 -> left, root2 -> left);
-    bool right = identicalTrees(root1 -> right, root2 -> right);
-    bool value = root1 -> data == root2 -> data;
-
-    if(left && right && value) {
-        return true;
-    }
-    else{
-        return false;
-    }
-}
+};
