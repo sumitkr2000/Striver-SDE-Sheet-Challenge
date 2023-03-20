3299c4325a6c49e504a84072914697d04186204a
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:57:11 +0530

    Update Matrix Median.cpp

--- Day 11/Matrix Median.cpp
+++ Day 11/Matrix Median.cpp
@@ -1,36 +1,37 @@
-int getMedian(vector<vector<int>> &matrix) {
-    int n = matrix.size();
-    int m = matrix[0].size();
-    
-    int low = 0;
-    int high = 1e9;
-    
-    while(low <= high) {
-        int mid1 = low + (high - low)/2;
-        int cnt = 0;
-        
-        for(int i = 0; i < n; i++) {
-            int s = 0, e = m-1;
-            
-            while(s <= e) {
-                int mid2 = s + (e - s)/2;
-                
-                if(matrix[i][mid2] <= mid1) {
-                    s = mid2 + 1;
+class Solution {
+public:
+    int findMedian(vector<vector<int> > &A) {
+
+        int n = A.size(, m = A[0].size();
+        int low = 0, high = 1e9;
+
+        while(low <= high) {
+            int mid1 = low + (high - low)/2;
+            int cnt = 0;
+
+            for(int i = 0; i < n; i++) {
+                int s = 0, e = m-1;
+
+                while(s <= e) {
+                    int mid2 = s + (e - s)/2;
+
+                    if(A[i][mid2] <= mid1) {
+                        s = mid2 + 1;
+                    }
+                    else {
+                        e = mid2 - 1;
+                    }
                 }
-                else {
-                    e = mid2 - 1;
-                }
+                cnt += s;
             }
-            cnt += s;
+
+            if(cnt <= (n*m)/2) {
+                low = mid1 + 1;
+            }
+            else {
+                high = mid1 - 1;
+            }
         }
-        
-        if(cnt <= (n*m)/2) {
-            low = mid1 + 1;
-        }
-        else {
-            high = mid1 - 1;
-        }
+        return low;
     }
-    return low;
-}
+};
