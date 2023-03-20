2060f483bfc1fe5a122044dfd388cd5c784d265a
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:30:56 +0530

    Update Height of the Binary Tree From Inorder and Level Order Traversal.cpp

--- Day 18/Height of the Binary Tree From Inorder and Level Order Traversal.cpp
+++ Day 18/Height of the Binary Tree From Inorder and Level Order Traversal.cpp
@@ -1,12 +1,14 @@
-int heightOfTheTree(vector<int>& inorder, vector<int>& levelOrder, int N){
-	
-    if(root == NULL) {
-        return 0;
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        
+        if(root == NULL) {
+            return 0;
+        }
+
+        int left = maxDepth(root -> left);
+        int right = maxDepth(root -> right);
+
+        return 1 + max(left, right);
     }
-
-    //recursive call
-    int left = maxDepth(root -> left);
-    int right = maxDepth(root -> right);
-
-    return 1 + max(left,right);
-}
+};
