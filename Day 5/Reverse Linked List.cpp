ba437afb86b8cd54728ca97d6983f5872c29b2fb
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:28:57 +0530

    Update Reverse Linked List.cpp

--- Day 5/Reverse Linked List.cpp
+++ Day 5/Reverse Linked List.cpp
@@ -1,19 +1,40 @@
-LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
-{
-    // Write your code here
-	LinkedListNode<int>* prev = NULL;
-	LinkedListNode<int>* curr = head;
-	LinkedListNode<int>* forward = NULL;
-	
-	while(curr != NULL) {
-		
-		forward = curr -> next;
-		curr -> next = prev;
-		prev = curr;
-		curr = forward;
-	}
-	
-	head = prev;
-	
-	return prev;
-}
+class Solution {
+private:
+    //Approach 2: Recursive
+    ListNode* reverse(ListNode* head) {
+        //base case
+        if(head == NULL || head -> next == NULL) {
+            return head;
+        }
+        
+        ListNode* newHead = reverse(head -> next);
+        
+        ListNode* curr = head -> next;
+        curr -> next = head;
+        head -> next = NULL;
+        return newHead;
+    }
+    
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        //Approach 1: Iterative
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* forward = NULL;
+
+        while(curr != NULL) {
+            forward = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = forward;
+        }
+
+        head = prev;
+        return prev;
+        
+        /*//Approach 2: Recursive
+        return reverse(head);
+        */
+    }
+};
