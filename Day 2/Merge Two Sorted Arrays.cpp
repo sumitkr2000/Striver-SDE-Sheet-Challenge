dd930109dc0de22c28e3469eeb451a8abb3e9129
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:13:45 +0530

    Update Merge Two Sorted Arrays.cpp

--- Day 2/Merge Two Sorted Arrays.cpp
+++ Day 2/Merge Two Sorted Arrays.cpp
@@ -1,28 +1,25 @@
-#include <bits/stdc++.h> 
-vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
-	
-    vector<int> v;
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-    int i = 0;
-    int j = 0;
-
-    while(i < m && j < n) {
-        if(arr1[i] < arr2[j]) {
-            v.push_back(arr1[i]);
-            i++;
+        int i = m-1, j = n-1, k = m+n-1;
+        
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
         }
-        else {
-            v.push_back(arr2[j]);
-            j++;
+        
+        while(j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
-    while(i < m) {
-        v.push_back(arr1[i]);
-        i++;
-    }
-    while(j < n) {
-        v.push_back(arr2[j]);
-        j++;
-    }
-    return v;
-}
+};
