3dd9c79fdad225c560700eff0c555127e26ac860
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:51:45 +0530

    Update Rat in a Maze Problem.cpp

--- Day 10/Rat in a Maze Problem.cpp
+++ Day 10/Rat in a Maze Problem.cpp
@@ -1,69 +1,63 @@
 class Solution{
-    private:
+private:
+    bool isSafe(int i, int j, int n, vector<vector<int>> &m) {
 
-    bool isSafe(vector<vector<int>> &m, int n, vector<vector<bool>> &visited, int x, int y) {
-
-        if((x >= 0 && x < n) && (y >= 0 && y < n) && (visited[x][y] == 0) && (m[x][y] == 1)) {
+        if(i >= 0 && i < n && j >= 0 && j < n && m[i][j] == 1) {
             return true;
         }
-        else {
-            return false;
-        }
+        return false;
     }
 
-    void solve(vector<vector<int>> &m, int n, vector<vector<bool>> &visited, string path, int x, int y, vector<string> &ans) {
-
-        //base case
-        if(x == n-1 && y == n-1) {
+    void solve(int i, int j, int n, vector<vector<int>> &m, string &path, vector<string> &ans) {
+        
+        if(i == n-1 && j == n-1) {
             ans.push_back(path);
             return;
         }
 
-        visited[x][y] = 1;
+        m[i][j] = 0;
 
         //down
-        if(isSafe(m, n, visited, x+1, y)) {
+        if(isSafe(i+1, j, n, m)) {
             path.push_back('D');
-            solve(m, n, visited, path, x+1, y, ans);
+            solve(i+1, j, n, m, path, ans);
             path.pop_back();
         }
         
         //left
-        if(isSafe(m, n, visited, x, y-1)) {
+        if(isSafe(i, j-1, n, m)) {
             path.push_back('L');
-            solve(m, n, visited, path, x, y-1, ans);
+            solve(i, j-1, n, m, path, ans);
             path.pop_back();
         }
         
         //right
-        if(isSafe(m, n, visited, x, y+1)) {
+        if(isSafe(i, j+1, n, m)) {
             path.push_back('R');
-            solve(m, n, visited, path, x, y+1, ans);
+            solve(i, j+1, n, m, path, ans);
             path.pop_back();
         }
 
         //up
-        if(isSafe(m, n, visited, x-1, y)) {
+        if(isSafe(i-1, j, n, m)) {
             path.push_back('U');
-            solve(m, n, visited, path, x-1, y, ans);
+            solve(i-1, j, n, m, path, ans);
             path.pop_back();
         }
 
-        visited[x][y]=0;
+        m[i][j] = 1;
     }
     
     public:
     vector<string> findPath(vector<vector<int>> &m, int n) {
         
-       vector<string> ans;
-       string path = "";
-       vector<vector<bool>> visited(n, vector<bool> (n, 0));
-
-       if(m[0][0] == 0) {
-           return ans;
-       }
-
-       solve(m, n, visited, path, 0, 0, ans);
-       return ans;
+        vector<string> ans;
+        string path = "";
+        
+        if(m[0][0] == 1) {
+           solve(0, 0, n, m, path, ans);
+        }
+        
+        return ans;
     }
 };
