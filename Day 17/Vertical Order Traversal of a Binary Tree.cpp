7264f97e16816027c9aec7d92bb685d3dab59752
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:22:50 +0530

    Update Vertical Order Traversal of a Binary Tree.cpp

--- Day 17/Vertical Order Traversal of a Binary Tree.cpp
+++ Day 17/Vertical Order Traversal of a Binary Tree.cpp
@@ -7,36 +7,37 @@ public:
             return ans;
         }
         
-        map<int, map<int, multiset<int> > > mp;
-        queue<pair< TreeNode*, pair<int, int> > > q;
+        map<int, map<int, multiset<int>>> mp;
+        queue<pair<TreeNode*, pair<int, int>>> q;
         
-        q.push(make_pair(root, make_pair(0, 0)));
+        q.push({root, {0, 0}});
         
         while(!q.empty()) {
-            
-            pair< TreeNode*, pair<int, int> > temp = q.front();
+            auto temp = q.front();
             q.pop();
             
             TreeNode* frontNode = temp.first;
-            int hd = temp.second.first;
-            int level = temp.second.second;
+            int hl = temp.second.first;
+            int vl = temp.second.second;
             
-            mp[hd][level].insert(frontNode -> val);
+            mp[hl][vl].insert(frontNode -> val);
             
             if(frontNode -> left) {
-                q.push(make_pair(frontNode -> left, make_pair(hd-1, level+1)));
+                q.push({frontNode -> left, {hl-1, vl+1}});
             }
             if(frontNode -> right) {
-                q.push(make_pair(frontNode -> right, make_pair(hd+1, level+1)));
+                q.push({frontNode -> right, {hl+1, vl+1}});
             }
         }
         
-        for(auto i: mp) {
-            vector<int> col;
-            for(auto j: i.second) {
-                    col.insert(col.end(), j.second.begin(), j.second.end());
+        for(auto it: mp) {
+            vector<int> temp;
+            for(auto j: it.second) {
+                for(auto k: j.second) {
+                    temp.push_back(k);
+                }
             }
-            ans.push_back(col);
+            ans.push_back(temp);
         }
         return ans;
     }
