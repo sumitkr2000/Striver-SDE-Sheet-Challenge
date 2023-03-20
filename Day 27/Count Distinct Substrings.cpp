57c76cd0efc85b050efaf2f40e7e9680c68b5025
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 15:57:07 +0530

    Update Count Distinct Substrings.cpp

--- Day 27/Count Distinct Substrings.cpp
+++ Day 27/Count Distinct Substrings.cpp
@@ -7,41 +7,39 @@ public:
             children[i] = NULL;
         }
     }
-    
-    void insert(int i, string &word, TrieNode* root, int &cnt) {
-        
-        if(i == word.size()) {
-            return;
-        }
-        
-        int ind = word[i] - 'a';
-        TrieNode* child;
-        
-        if(root -> children[ind]) {
-            child = root -> children[ind];
-        }
-        else {
-            child = new TrieNode();
-            root -> children[ind] = child;
-            cnt++;
-        }
-        
-        insert(i+1, word, child, cnt);
-    }
 };
 
-int countDistinctSubstrings(string &s)
-{
-    int n = s.size();
-    TrieNode* root = new TrieNode();
-    int cnt = 0;
-    
-    for(int i = 0; i < n; i++) {
-        string temp = "";
-        for(int j = i; j < n; j++) {
-            temp += s[j];
-            root -> insert(0, temp, root, cnt);
+class Solution {
+private:
+    void insert(int i, string &word, TrieNode* root, int &cnt) {
+        
+        TrieNode* curr = root;
+        
+        for(auto &it: word) {
+            int ind = it - 'a';
+            
+            if(!curr -> children[ind]) {
+                curr -> children[ind] = new TrieNode();
+                cnt++;
+            }
+            curr = curr -> children[ind];
         }
     }
-    return cnt + 1;
-}
+    
+public:
+    int countDistinctSubstrings(string &s) {
+        
+        int n = s.size();
+        TrieNode* root = new TrieNode();
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++) {
+            string temp = "";
+            for(int j = i; j < n; j++) {
+                temp += s[j];
+                insert(0, temp, root, cnt);
+            }
+        }
+        return cnt + 1;
+    }
+};
