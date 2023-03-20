1b5379d236bd2da9618e77f99d035c2b53d5e440
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 15:19:49 +0530

    Update Floyd Warshall Algorithm.cpp

--- Day 24/Floyd Warshall Algorithm.cpp
+++ Day 24/Floyd Warshall Algorithm.cpp
@@ -1,21 +1,17 @@
 class Solution {
-  public:
+public:
 	void shortest_distance(vector<vector<int>>&matrix){
-	    
+
 	    int n = matrix.size();
+
 	    for(int k = 0; k < n; k++) {
-	        for(int i = 0; i < n; i++) {
-    	        for(int j = 0; j < n; j++) {
-    	            if(matrix[i][k] != -1 && matrix[k][j] != -1) {
-    	                if(matrix[i][j] == -1) {
-    	                    matrix[i][j] = matrix[i][k] + matrix[k][j];
-    	                }
-    	                else {
-        	                matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
-    	                }
-    	            }
-    	        }
-    	    }
+		for(int i = 0; i < n; i++) {
+		    for(int j = 0; j < n; j++) {
+			if(matrix[i][k] != INT_MAX && matrix[k][j] != INT_MAX) {
+			    matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+			}
+		    }
+		}
 	    }
 	}
 };
