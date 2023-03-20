161be0f6ef0f155dbc990e873708f87e86604e48
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 15:32:10 +0530

    Update Longest Common Subsequence.cpp

--- Day 25/Longest Common Subsequence.cpp
+++ Day 25/Longest Common Subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    int solve(int i, int j, string text1, string &text2,vector<vector<int>> &dp) {
+    int solve(int i, int j, string &text1, string &text2, vector<vector<int>> &dp) {
         
         if(i == text1.size() || j == text2.size()) {
             return 0;
@@ -22,12 +22,12 @@ public:
         
         int n = text1.size(), m = text2.size();
         
-        //Approach 1: Memoization
+        // Approach 1: Memoization
         vector<vector<int>> dp(n, vector<int>(m, -1));            
         return solve(0, 0, text1, text2, dp);
         
         
-        /*//Approach 2: Tabulation
+        /* // Approach 2: Tabulation
         vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
         
         for(int i = n-1; i >= 0; i--) {
@@ -44,9 +44,8 @@ public:
         return dp[0][0];
         */
         
-        /*//Approach 3: Space optimized
-        vector<int> next(m+1, 0);
-        vector<int> curr(m+1, 0);
+        /* // Approach 3: Space optimized
+        vector<int> next(m+1, 0), curr(m+1, 0);
         
         for(int i = n-1; i >= 0; i--) {
             for(int j = m-1; j >= 0; j--) {
