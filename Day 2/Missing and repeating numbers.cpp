3faf49528eed5cd0b8778cc951e59f6002cfdcf2
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:15:51 +0530

    Update Missing and repeating numbers.cpp

--- Day 2/Missing and repeating numbers.cpp
+++ Day 2/Missing and repeating numbers.cpp
@@ -1,21 +1,50 @@
-#include <bits/stdc++.h>
-pair<int,int> missingAndRepeating(vector<int> &arr, int n) {
-    
-	pair<int,int> ans;
-    unordered_map<int, pair<int, bool>> mp;
-    
-    for(int i = 0; i < n; i++) {
-        mp[arr[i]].first++;
-        mp[arr[i]].second = true;
-    }
-    
-    for(int i = 1; i <= n; i++) {
-        if(mp[i].second == 0) {
-            ans.first = i;
-        }
-        if(mp[i].first > 1) {
-            ans.second = i;
-        }
-    } 
-    return ans;
-}
+class Solution {
+public:
+	vector<int> Solution::repeatedNumber(const vector<int> &A) {
+
+	    int n = A.size();
+	    int xor1 = A[0];
+
+	    for (int i = 1; i < n; i++) {
+		xor1 = xor1^A[i];
+
+	    }
+	    for (int i = 1; i <= n; i++) {
+		xor1 = xor1^i;
+	    }
+
+	    int set_bit_no = xor1 & ~(xor1-1);
+	    int x = 0, y = 0;
+
+	    for (int i = 0; i < n; i++) {
+		if(A[i] & set_bit_no) {
+		    x = x^A[i];
+		}
+		else {
+		    y = y^A[i];
+		}
+	    }
+
+	    for(int i = 1; i <= n; i++) {
+		if(i & set_bit_no) {
+		    x = x^i;
+		}
+		else {
+		    y = y^i;
+		}
+	    }
+
+	    int x_count = 0;
+	    for(int i = 0; i < n1; i++) {
+		if(A[i] == x) {
+		    x_count++;
+		}
+	    }
+
+	    if(x_count == 0) {
+		return {y, x};
+	    }
+
+	    return {x, y};
+	}
+}:
