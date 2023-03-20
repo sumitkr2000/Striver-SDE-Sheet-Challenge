3d31d6cbf77389d18582652b53c305d996bae029
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:09:00 +0530

    Update Valid Anagram.cpp

--- Day 16/Valid Anagram.cpp
+++ Day 16/Valid Anagram.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        vector<int> s1(26, 0), t1(26, 0);
+        vector<int> f1(26, 0), f2(26, 0);
         
-        if(s.length() != t.length()) return false;
+        if(s.size() != t.size()) {
+            return false;
+        }
         
         for(int i = 0; i < s.size(); i++) {
-            int index1 = s[i] - 'a';
-            s1[index1]++;
-            
-            int index2 = t[i] - 'a';
-            t1[index2]++; 
+            f1[s[i] - 'a']++;
+            f2[t[i] - 'a']++; 
         }
         
-        for(int i = 0; i < 26; i++) {
-            if(s1[i] != t1[i]) {
-                return false;
-            }
-        }
-        return true;
+        return f1 == f2;
     }
 };
