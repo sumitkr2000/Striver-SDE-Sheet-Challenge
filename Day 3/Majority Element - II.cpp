a50289dc36138105a9a5c9203eb54aec0cac110e
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:16:35 +0530

    Update Majority Element - II.cpp

--- Day 3/Majority Element - II.cpp
+++ Day 3/Majority Element - II.cpp
@@ -1,19 +1,49 @@
-#include <bits/stdc++.h> 
-vector<int> majorityElementII(vector<int> &arr)
-{ 
-    int n = arr.size(); 
-    map<int, int> mp;
-
-    for(int i = 0; i < n; i++) {
-        mp[arr[i]]++;
-    }
-
-    vector<int> ans;
-
-    for(auto i: mp) {
-        if(i.second > n/3) {
-            ans.push_back(i.first);
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        
+        int num1 = 0, cnt1 = 0;
+        int num2 = 0, cnt2 = 0;
+        
+        for(auto it: nums) {
+            if(it == num1) {
+                cnt1++;
+            }
+            else if(it == num2) {
+                cnt2++;
+            }
+            else if(cnt1 == 0) {
+                num1 = it;
+                cnt1 = 1;
+            }
+            else if(cnt2 == 0) {
+                num2 = it;
+                cnt2 = 1;
+            }
+            else {
+                cnt1--;
+                cnt2--;
+            }
         }
+        
+        vector<int> ans;  
+        cnt1 = 0, cnt2 = 0;
+        
+        for(auto it: nums) {
+            if(it == num1) {
+                cnt1++;
+            }
+            else if(it == num2) {
+                cnt2++;
+            }
+        }
+        
+        if(cnt1 > nums.size()/3) {
+            ans.push_back(num1);
+        }
+        if(cnt2 > nums.size()/3) {
+            ans.push_back(num2);
+        }
+        return ans;
     }
-    return ans;
-}
+};
