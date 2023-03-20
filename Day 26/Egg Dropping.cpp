39ac1b6c8325431123eee000a435c73ab150b937
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 15:41:07 +0530

    Update and rename Egg Dropping Puzzle.cpp to Egg Dropping.cpp

--- Day 26/Egg Dropping.cpp
+++ Day 26/Egg Dropping.cpp
@@ -1,6 +1,5 @@
-class Solution
-{
-    private:
+class Solution {
+private:
     int solve(int n, int k, vector<vector<int>> &dp) {
         
         if(k == 0 || k == 1 || n == 1) {
@@ -12,6 +11,7 @@ class Solution
         }
         
         int mini = INT_MAX;
+        
         for(int i = 1; i <= k; i++) {
             int temp = 1 + max(solve(n-1, i-1, dp), solve(n, k-i, dp));
             mini = min(mini, temp);
@@ -20,9 +20,9 @@ class Solution
         return dp[n][k] = mini;
     }
     
-    public:
-    int eggDrop(int n, int k) 
-    {
+public:
+    int eggDrop(int n, int k) {
+        
         vector<vector<int>> dp(n+1, vector<int>(k+1, -1));
         return solve(n, k, dp);
     }
