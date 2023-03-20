ab5a6379308628ab96b42d8c8f1402a7b4fe3418
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 15:27:57 +0530

    Update 0 - 1 Knapsack.cpp

--- Day 25/0 - 1 Knapsack.cpp
+++ Day 25/0 - 1 Knapsack.cpp
@@ -1,6 +1,5 @@
-class Solution
-{
-    private:
+class Solution {
+private:
     int solve(int i, int W, int n, int wt[], int val[], vector<vector<int>> &dp) {
         
         if(i == n) {
@@ -13,6 +12,7 @@ class Solution
         
         int notTake = solve(i+1, W, n, wt, val, dp);
         int take = INT_MIN;
+        
         if(wt[i] <= W) {
             take = val[i] + solve(i+1, W-wt[i], n, wt, val, dp);
         }
@@ -20,10 +20,9 @@ class Solution
         return dp[i][W] = max(take, notTake);
     }
     
-    public:
-    //Function to return max value that can be put in knapsack of capacity W.
-    int knapSack(int W, int wt[], int val[], int n) 
-    { 
+public:
+    int knapSack(int W, int wt[], int val[], int n) {
+        
         //Approach 1: Memoization
         vector<vector<int>> dp(n, vector<int>(W+1, -1));
         return solve(0, W, n, wt, val, dp);
@@ -36,6 +35,7 @@ class Solution
             for(int j = W; j >= 0; j--) {
                 int notTake = dp[i+1][j];
                 int take = INT_MIN;
+                
                 if(wt[i] <= j) {
                     take = val[i] + dp[i+1][j-wt[i]];
                 }
@@ -47,13 +47,13 @@ class Solution
         */
         
         /*//Approach 3: Space optimized
-        vector<int> next(W+1, 0);
-        vector<int> curr(W+1, 0);
+        vector<int> next(W+1, 0), curr(W+1, 0);
         
         for(int i = n-1; i >= 0; i--) {
             for(int j = W; j >= 0; j--) {
                 int notTake = next[j];
                 int take = INT_MIN;
+                
                 if(wt[i] <= j) {
                     take = val[i] + next[j-wt[i]];
                 }
