cf1b277b72b5cdb3e2d59bbc7635c386c1b42fee
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 12:19:38 +0530

    Update Valid Parentheses.cpp

--- Day 13/Valid Parentheses.cpp
+++ Day 13/Valid Parentheses.cpp
@@ -1,35 +1,24 @@
-bool isValidParenthesis(string expression)
-{
-    stack<char> s;
-	
-	for(int i = 0; i < expression.size(); i++) {
-		
-		char ch = expression[i];
-		
-		if(ch == '(' || ch == '{' || ch == '[') {
-			s.push(ch);
-		}
-		else{
-			if(!s.empty()) {
-				char top = s.top();
-				if((ch == ')' && top == '(')||
-				   (ch == '}' && top == '{')||
-				   (ch == ']' && top == '[')) {
-					
-					s.pop();
-				}
-				else{
-					return false;
-				}
-			}
-			else{
-				return false;
-			}
-		}
-		
-	}
-	if(s.empty()) 
-	    return true;
-	else
-		return false;
-}
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> st;
+	    
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            }
+            else {
+                if(!st.empty() && ((s[i] == ')' && st.top() == '(') || 
+                    (s[i] == '}' && st.top() == '{') || 
+                    (s[i] == ']' && st.top() == '['))) {
+                        st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
