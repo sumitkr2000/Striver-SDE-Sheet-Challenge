e550fa786c7f078a891f34077088f603942d64c5
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:43:32 +0530

    Update Maximum Path Sum.cpp

--- Day 19/Maximum Path Sum.cpp
+++ Day 19/Maximum Path Sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 private:
     int solve(TreeNode* root, int &ans) {
-        //base case
+        
         if(root == NULL) {
             return 0;
         }
@@ -10,7 +10,6 @@ private:
         int right = solve(root -> right, ans);
         
         int maxi = max(root -> val, max(left, right) + root -> val);
-        
         ans = max(ans, max(maxi, root -> val + left + right));
         
         return maxi;
