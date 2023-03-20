22bc9ffbb902bec709e4c7e65233392b80866177
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:05:45 +0530

    Update Minimum Characters Required To Make A String Palindromic.cpp

--- Day 16/Minimum Characters Required To Make A String Palindromic.cpp
+++ Day 16/Minimum Characters Required To Make A String Palindromic.cpp
@@ -1,36 +1,40 @@
-int lps(string temp) {
-    
-    int n = temp.size();
-    vector<int> pi(n);
-    pi[0] = 0;
-    
-    int i = 0, j = 1;
-    
-    while(j < n) {
-        if(temp[i] == temp[j]) {
-            pi[j] = i+1;
-            i++;
-            j++;
-        }
-        else {
-            if(i == 0) {
-                pi[j] = 0;
+class Solution {
+private:
+    int lps(string temp) {
+
+        int n = temp.size();
+        vector<int> pi(n);
+        pi[0] = 0;
+
+        int i = 0, j = 1;
+
+        while(j < n) {
+            if(temp[i] == temp[j]) {
+                pi[j] = i+1;
+                i++;
                 j++;
             }
             else {
-                i = pi[i-1];
+                if(i == 0) {
+                    pi[j] = 0;
+                    j++;
+                }
+                else {
+                    i = pi[i-1];
+                }
             }
         }
+        return pi[n-1];
     }
-    return pi[n-1];
-}
 
-int Solution::solve(string A) {
-    
-    string B = A;
-    reverse(B.begin(), B.end());
-    
-    string temp = A + "$" + B;
-    
-    return A.size() - lps(temp);
-}
+public:
+    int solve(string A) {
+
+        string B = A;
+        reverse(B.begin(), B.end());
+
+        string temp = A + "$" + B;
+
+        return A.size() - lps(temp);
+    }
+};
