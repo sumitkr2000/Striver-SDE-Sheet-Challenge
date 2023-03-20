f6063c9f71f084adc6b76502bf4ad5770df3c0e3
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:57:38 +0530

    Update Median of two sorted arrays.cpp

--- Day 11/Median of two sorted arrays.cpp
+++ Day 11/Median of two sorted arrays.cpp
@@ -1,27 +1,40 @@
-#include<algorithm>
-double median(vector<int> a, vector<int> b)
-{
-    vector<double> c;
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
-        for(int i = 0; i < a.size(); i++) {
-            c.push_back(a[i]);
+        if(nums2.size() < nums1.size()) {
+            return findMedianSortedArrays(nums2, nums1);
         }
-        for(int j = 0; j < b.size(); j++) {
-            c.push_back(b[j]);
-        }
-        sort(c.begin(), c.end());
         
-        int s = 0;
-        int e = c.size()-1;
-        int mid = s + (e-s)/2;
-        double median = -1;
+        int n1 = nums1.size(), n2 = nums2.size();
+        int s = 0, e = n1;
         
-        if(c.size()%2 == 0) {
-           median = (c[mid] + c[mid+1])/2;
+        while(s <= e) {
+            
+            int cut1 = s + (e-s)/2;
+            int cut2 = (n1 + n2 + 1)/2 - cut1;
+            
+            int l1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int l2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
+            
+            int r1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int r2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+            
+            if(l1 <= r2 && l2 <= r1) {
+                if((n1+n2)%2 == 0) {
+                    return (max(l1, l2) + min(r1, r2))/2.0;
+                }
+                else {
+                    return max(l1, l2);
+                }
+            }
+            else if(l1 > r2) {
+                e = cut1 - 1;
+            }
+            else {
+                s = cut1 + 1;
+            }
         }
-        else{
-            median = c[mid];
-        }
-        return median;
-}
-
+        return 0;
+    }
+};
