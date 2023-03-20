5516357d8b3bef13a521830f7adbadd748e50a9f
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 15:35:18 +0530

    Update Matrix Chain Multiplication.cpp

--- Day 25/Matrix Chain Multiplication.cpp
+++ Day 25/Matrix Chain Multiplication.cpp
@@ -11,8 +11,9 @@ private:
         }
         
         int mini = INT_MAX;
+        
         for(int k = i; k < j; k++) {
-            int steps = arr[i-1]*arr[k]*arr[j] + solve(i, k, arr, dp) + solve(k+1, j, arr, dp);
+            int steps = arr[i-1] * arr[k] * arr[j] + solve(i, k, arr, dp) + solve(k+1, j, arr, dp);
             mini = min(mini, steps);
         }
         
@@ -20,8 +21,8 @@ private:
     }
 
 public:
-    int matrixMultiplication(int N, int arr[])
-    {
+    int matrixMultiplication(int N, int arr[]) {
+        
         vector<vector<int>> dp(N, vector<int>(N, -1));
         return solve(1, N-1, arr, dp);
     }
