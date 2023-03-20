f92bb9b00e73970642c543f7a7d0987c641545af
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:31:18 +0530

    Update Intersection of Two Linked Lists.cpp

--- Day 6/Intersection of Two Linked Lists.cpp
+++ Day 6/Intersection of Two Linked Lists.cpp
@@ -1,32 +1,24 @@
-#include <bits/stdc++.h> 
-int findIntersection(Node *firstHead, Node *secondHead)
-{
-    if(firstHead == NULL || secondHead == NULL) {
-        return NULL;
-    }
-    
-    Node* temp1 = firstHead;
-    Node* temp2 = secondHead;
-
-    while(temp1 != temp2) {
-        if(temp1 == NULL) {
-            temp1 = secondHead;
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        ListNode *temp1 = headA, *temp2 = headB;
+        
+        while(temp1 != temp2) {
+            if(temp1 == NULL) {
+                temp1 = headB;
+            }
+            else{
+                temp1 = temp1 -> next;
+            }
+            
+            if(temp2 == NULL) {
+                temp2 = headA;
+            }
+            else{
+                temp2 = temp2 -> next;
+            }
         }
-        else{
-            temp1 = temp1 -> next;
-        }
-
-        if(temp2 == NULL) {
-            temp2 = firstHead;
-        }
-        else{
-            temp2 = temp2 -> next;
-        }
+        return temp1;
     }
-    if(temp1 == NULL) {
-        return -1;
-    }
-    else{
-        return temp1->data;
-    }
-}
+};
