e51f8fbd2a6a104e7415d07d2f2fe2bb198944c2
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:55:48 +0530

    Update Kth Element of Two Sorted Arrays.cpp

--- Day 11/Kth Element of Two Sorted Arrays.cpp
+++ Day 11/Kth Element of Two Sorted Arrays.cpp
@@ -1,24 +1,32 @@
-#include<bits/stdc++.h>
-int ninjaAndLadoos(vector<int> &row1, vector<int> &row2, int m, int n, int k) {
-    
-    if(n > m)
-        return ninjaAndLadoos(row2, row1, n, m, k);
-    
-    int low = max(0, k-n), high = min(k, m);
-    while(low <= high){
-        int cut1 = (low+high) >> 1;
-        int cut2 = k-cut1;
-        int l1 = (cut1 == 0)? INT_MIN: row1[cut1-1];
-        int l2 = (cut2 == 0)? INT_MIN: row2[cut2-1];
-        int r1 = (cut1 == m)? INT_MAX: row1[cut1];
-        int r2 = (cut2 == n)? INT_MAX: row2[cut2];
+class Solution{
+public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k) {
         
-        if(l1 <= r2 && l2 <= r1)
-            return max(l1, l2);
-        else if(l1 > r2)
-            high = cut1-1;
-        else if(l2 > r1)
-                low = cut1+1;
+        if(m < n) {
+            return kthElement(arr2, arr1, m, n, k);
+        }
+        
+        int s = max(0, k-m), e = min(n, k);
+        
+        while(s <= e) {
+            int cut1 = s + (e-s)/2;
+            int cut2 = k - cut1;
+            
+            int l1 = cut1 == 0 ? INT_MIN : arr1[cut1-1];
+            int l2 = cut2 == 0 ? INT_MIN : arr2[cut2-1];
+            
+            int r1 = cut1 == n ? INT_MAX : arr1[cut1];
+            int r2 = cut2 == m ? INT_MAX : arr2[cut2];
+            
+            if(l1 <= r2 && l2 <= r1) {
+                return max(l1, l2);
+            }
+            else if(l1 > r2) {
+                e = cut1 - 1;
+            }
+            else {
+                s = cut1 + 1;
+            }
+        }
     }
-    return 1;
-}
+};
