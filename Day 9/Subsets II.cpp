bc8aa52a0e0eb9aa49b2e48c10102604d42aba26
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:47:12 +0530

    Update Subsets II.cpp

--- Day 9/Subsets II.cpp
+++ Day 9/Subsets II.cpp
@@ -1,41 +1,27 @@
-#include<bits/stdc++.h>
-void solve(vector<int> &arr, int i, vector<int> &output, vector<vector<int>> &res) {
-        //base case
-        if(i == arr.size()) {
-            sort(output.begin(), output.end());
-            res.push_back(output);
-            return;
+class Solution {
+private:
+    void solve(int i, vector<int> &nums, vector<int> &op, vector<vector<int>> &ans) {
+        
+        ans.push_back(op);
+        
+        for(int j = i; j < nums.size(); j++) {
+            if(j != i && nums[j] == nums[j-1]) {
+                continue;
+            }
+            op.push_back(nums[j]);
+            solve(j + 1, nums, op, ans);
+            op.pop_back();
         }
-        
-        vector<int> op1 = output;
-        vector<int> op2 = output;
-        
-        op2.push_back(arr[i]);
-        
-        solve(arr, i+1, op1, res);
-        solve(arr, i+1, op2, res);
-        
-        return;
     }
-
-vector<vector<int>> uniqueSubsets(int n, vector<int> &arr)
-{
-    vector<vector<int>> res;
-    vector<int> output;
-    int i = 0;
-    solve(arr, i, output, res);
-
-    set<vector<int>> st;        
-    for(int i = 0; i < res.size(); i++) {
-        st.insert(res[i]);
+    
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+        vector<vector<int>> ans;
+        vector<int> op;
+        sort(nums.begin(), nums.end());
+        solve(0, nums, op, ans);
+        
+        return ans;
     }
-
-    vector<vector<int>> ans;
-
-    set<vector<int>>:: iterator it;
-    for(it = st.begin(); it != st.end(); it++){
-        vector<int> temp = *it;
-        ans.push_back(temp);
-    }        
-    return ans;
-}
+};
