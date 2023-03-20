973912f1d055d5a34ac854b6482e470351f0ccd1
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:04:34 +0530

    Update Maximum Subarray Sum.cpp

--- Day 1/Maximum Subarray Sum.cpp
+++ Day 1/Maximum Subarray Sum.cpp
@@ -1,8 +1,6 @@
-#include <bits/stdc++.h> 
-long long maxSubarraySum(int arr[], int n)
-{
-   long long ans = 0;
-   long long sum = 0;
+long long maxSubarraySum(int arr[], int n) {
+   
+   long long ans = 0, sum = 0;
 
    for(int i = 0; i < n; i++) {
        sum += arr[i];       
