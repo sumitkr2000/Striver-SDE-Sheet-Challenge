7c62d090433c5d7868d0016bda59db6df93b0092
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:34:55 +0530

    Update Level Order Traversal.cpp

--- Day 18/Level Order Traversal.cpp
+++ Day 18/Level Order Traversal.cpp
@@ -1,32 +1,33 @@
-vector<int> getLevelOrder(BinaryTreeNode<int> *root)
-{
-    vector<int> ans;
-    if(root == NULL) {
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        
+        vector<vector<int>> ans;
+        if(root == NULL) {
+            return ans;
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()) {
+            int size = q.size();
+            vector<int> v;
+            
+            for(int i = 0; i < size; i++) {
+                TreeNode *temp = q.front();
+                q.pop();
+                v.push_back(temp -> val);
+                
+                if(temp -> left) {
+                    q.push(temp -> left);
+                }
+                if(temp -> right) {
+                    q.push(temp -> right);
+                }
+            }
+            ans.push_back(v);
+        }
         return ans;
     }
-
-    queue<BinaryTreeNode<int>*> q;
-    q.push(root);
-    q.push(NULL);
-    while(!q.empty()) {
-
-        BinaryTreeNode<int> *temp = q.front();
-        q.pop();
-
-        if(temp == NULL) {
-            if(!q.empty()) {
-                q.push(NULL);
-            }
-        }
-        else{
-            ans.push_back(temp -> val);
-            if(temp -> left) {
-                q.push(temp -> left);
-            }
-            if(temp -> right) {
-                q.push(temp -> right);
-            }
-        }
-    }
-    return ans;
-}
+};
