844ee3fc375ccadb7e502591af58b86ac7a82706
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:08:47 +0530

    Update Rotate Matrix.cpp

--- Day 2/Rotate Matrix.cpp
+++ Day 2/Rotate Matrix.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         
-        int row = matrix.size();
-        int col = matrix[0].size();
+        int row = matrix.size(), col = matrix[0].size();
         
         for(int i = 0; i < row; i++) {           
             for(int  j = i; j < col; j++) {                
