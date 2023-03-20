8e413e6e46a95f7af866a636a97d4dca1822b568
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:53:11 +0530

    Update Aggressive Cows.cpp

--- Day 11/Aggressive Cows.cpp
+++ Day 11/Aggressive Cows.cpp
@@ -1,41 +1,39 @@
-bool isPossible(vector<int> &positions, int c, int mid) {
-    
-    int playerCount = 1;
-    int lastRoom = positions[0];
-    
-    for(int i = 0; i < positions.size(); i++) {
-        if(positions[i]-lastRoom >= mid) {
-            playerCount = playerCount + 1;
-            if (playerCount == c) {
-                return true;
+class Solution {
+private:
+    bool isPossible(vector<int> &stalls, int n, int k, int mid) {
+        
+        int cnt = 1, last = stalls[0];
+        for(int i = 1; i < n; i++) {
+            if(stalls[i] - last >= mid) {
+                cnt++;
+                last = stalls[i];
+                
+                if(cnt == k) {
+                    return true;
+                }
             }
-            lastRoom = positions[i];
         }
+        return false;
     }
-    return false;
-}
-
-int chessTournament(vector<int> positions , int n ,  int c){
 	
-	sort(positions.begin(), positions.end());
-    int s = 0;
-    int maxi = -1;
-    for(int i = 0; i < positions.size(); i++) {
-        maxi = max(maxi, positions[i]);
-    }
-    int e = maxi;
-    int ans = -1;
-    int mid = s + (e-s)/2;
+public:
+    int solve(int n, int k, vector<int> &stalls) {
     
-    while(s<=e) {
-    if (isPossible(positions, c, mid)) {
-        ans = mid;
-        s = mid + 1;
-    }
-        else{
-            e = mid - 1;
+        sort(stalls.begin(), stalls.end());
+        int s = 1, e = stalls[n-1] - stalls[0];
+        int ans = 0;
+        
+        while(s <= e) {
+            int mid = s + (e - s)/2;
+            
+            if(isPossible(stalls, n, k, mid)) {
+                ans = mid;
+                s = mid+1;
+            }
+            else {
+                e = mid-1;
+            }
         }
-        mid = s + (e-s)/2;
-  }
-    return ans;
-}
+        return ans;
+    }
+};
