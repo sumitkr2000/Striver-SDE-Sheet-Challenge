55dc4e953ba622053fcadc215dc90a0263b31c86
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:37:58 +0530

    Update Children Sum Property.cpp

--- Day 19/Children Sum Property.cpp
+++ Day 19/Children Sum Property.cpp
@@ -1,40 +1,43 @@
-void changeTree(BinaryTreeNode<int>* root) {
-    
-    if(root == NULL) {
-        return;
-    }
-    
-    int childSum = 0;
-    if(root -> left) {
-        childSum += root -> left -> data;
-    }
-    if(root -> right) {
-        childSum += root -> right -> data;
-    }
-    
-    if(root -> data <= childSum) {
-        root -> data = childSum;
-    }
-    else {
+class Solution {
+public:
+    void changeTree(TreeNode<int>* root) {
+
+        if(root == NULL) {
+            return;
+        }
+
+        int childSum = 0;
         if(root -> left) {
-            root -> left -> data = root -> data;
+            childSum += root -> left -> data;
         }
         if(root -> right) {
-            root -> right -> data = root -> data;
+            childSum += root -> right -> data;
+        }
+
+        if(root -> data <= childSum) {
+            root -> data = childSum;
+        }
+        else {
+            if(root -> left) {
+                root -> left -> data = root -> data;
+            }
+            if(root -> right) {
+                root -> right -> data = root -> data;
+            }
+        }
+
+        changeTree(root -> left);
+        changeTree(root -> right);
+
+        int sum = 0;
+        if(root -> left) {
+            sum += root -> left -> data;
+        }
+        if(root -> right) {
+            sum += root -> right -> data;
+        }
+        if(root -> left || root -> right) {
+            root -> data = sum;
         }
     }
-    
-    changeTree(root -> left);
-    changeTree(root -> right);
-    
-    int sum = 0;
-    if(root -> left) {
-        sum += root -> left -> data;
-    }
-    if(root -> right) {
-        sum += root -> right -> data;
-    }
-    if(root -> left || root -> right) {
-        root -> data = sum;
-    }
-}
+};
