3a02fa71fb174aa79a1c74e6c935cb46299de17e
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:12:30 +0530

    Update Bottom View of Binary Tree .cpp

--- Day 17/Bottom View of Binary Tree .cpp
+++ Day 17/Bottom View of Binary Tree .cpp
@@ -9,11 +9,9 @@ class Solution {
         
         map<int, int> mp;
         queue<pair <Node*, int> > q;
-        
-        q.push(make_pair(root, 0));
+        q.push({root, 0});
         
         while(!q.empty()) {
-            
             pair<Node*, int> temp = q.front();
             q.pop();
             
@@ -23,15 +21,15 @@ class Solution {
             mp[hd] = frontNode -> data;
     
             if(frontNode -> left) {
-                q.push(make_pair(frontNode -> left, hd-1));
+                q.push({frontNode -> left, hd-1});
             }
             if(frontNode -> right) {
-                q.push(make_pair(frontNode -> right,hd+1));
+                q.push({frontNode -> right, hd+1});
             }
         }
         
-        for(auto i: mp) {
-            ans.push_back(i.second);
+        for(auto it: mp) {
+            ans.push_back(it.second);
         }
         return ans;
     }
