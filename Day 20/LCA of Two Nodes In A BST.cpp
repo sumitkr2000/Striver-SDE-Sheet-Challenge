9fb97498a116598536780ab8059809abb6714912
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:45:34 +0530

    Update LCA of Two Nodes In A BST.cpp

--- Day 20/LCA of Two Nodes In A BST.cpp
+++ Day 20/LCA of Two Nodes In A BST.cpp
@@ -1,32 +1,18 @@
-TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q)
-{
-    //Approach 1 using iteration
-    while(root != NULL) {
-        if(root -> data > P -> data && root -> data > Q -> data) {
-            root = root -> left;
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        while(root) {
+            if(root -> val > p -> val && root -> val > q -> val) {
+                root = root -> left;
+            }
+            else if(root -> val < p -> val && root -> val < q -> val) {
+                root = root -> right;
+            }
+            else {
+                return root;
+            }
         }
-        else if(root -> data < P -> data && root -> data < Q -> data) {
-            root = root -> right;
-        } 
-        else{
-            return root;
-        }     
+        return root;
     }
-    
-    /*
-    //Approach 2 using recursion
-	//base case
-    if(root == NULL) {
-        return NULL;
-    }
-    
-    if(root -> data > P -> data && root -> data > Q -> data) {
-        return LCAinaBST(root -> left, P, Q);
-    }
-    
-    if(root -> data < P -> data && root -> data < Q -> data) {
-        return LCAinaBST(root -> right, P, Q);
-    } 
-    return root;
-    */
-}
+};
