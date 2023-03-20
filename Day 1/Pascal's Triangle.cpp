8df96f9ea8c9a1dc305ff46ebd36095bb8f9d676
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:07:33 +0530

    Update Pascal's Triangle.cpp

--- Day 1/Pascal's Triangle.cpp
+++ Day 1/Pascal's Triangle.cpp
@@ -1,15 +1,18 @@
-vector<vector<long long int>> printPascal(int n) {
-  
-    vector<vector<long long int>> ans(n);
-        
-    for(int i = 0; i < n; i++) {
-      
-        ans[i].resize(i+1);
-        ans[i][0] = ans[i][i] = 1;
+class Solution {
+public:
+  vector<vector<long long int>> printPascal(int n) {
 
-        for(int j = 1; j < i; j++) {
-            ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
-        } 
-    }
-    return ans; 
-}
+      vector<vector<long long int>> ans(n);
+
+      for(int i = 0; i < n; i++) {
+
+          ans[i].resize(i+1);
+          ans[i][0] = ans[i][i] = 1;
+
+          for(int j = 1; j < i; j++) {
+              ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+          } 
+      }
+      return ans; 
+  }
+};
