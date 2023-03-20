bbd82190d38f8df1a6fc4f19cc77163c172c74ec
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 15:24:07 +0530

    Update MST using Prim’s Algo.cpp

--- Day 24/MST using Prim’s Algo.cpp
+++ Day 24/MST using Prim’s Algo.cpp
@@ -1,33 +1,27 @@
-int spanningTree(int V, vector<vector<int>> adj[])
-    {
-        vector<int> edgeWt(V, INT_MAX);
-        vector<bool> isMst(V, 0);
-        edgeWt[0] = 0;
+class Solution {
+public:
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        
+        vector<bool> vis(V, 0);
+        int ans = 0;
         
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         pq.push({0, 0});
         
         while(!pq.empty()) {
-            int node = pq.top().second;
+            pair<int, int> p = pq.top();
+            int node = p.second;
+            int wt = p.first;
             pq.pop();
-            isMst[node] = 1;
             
-            for(auto it: adj[node]) {
-                int v = it[0];
-                int w = it[1];
-                
-                if(!isMst[v] && w < edgeWt[v]) {
-                    edgeWt[v] = w;
-                    pq.push({w, v});
+            if(!vis[node]) {
+                vis[node] = 1;
+                ans += wt;
+                for(auto it: adj[node]) {
+                    pq.push({it[1], it[0]});
                 }
             }
         }
-        
-        int ans = 0;
-        for(auto it: edgeWt) {
-            ans += it;
-        }
-        
         return ans;
     }
 };
