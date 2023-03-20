868a86873341e358415c76274bc7a2f953951836
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:27:06 +0530

    Update Boundary Traversal of Binary Tree.cpp

--- Day 18/Boundary Traversal of Binary Tree.cpp
+++ Day 18/Boundary Traversal of Binary Tree.cpp
@@ -1,73 +1,78 @@
-void traverseLeft(TreeNode<int>* root, vector<int> &ans) {
-    //base case
-    if((root == NULL) || (root -> left == NULL && root -> right == NULL)) {
-        return;
+class Solution {
+private:
+    bool isLeaf(TreeNode<int>* root) {
+        return (root -> left == NULL && root -> right == NULL);
     }
 
-    ans.push_back(root -> data);
+    void traverseLeft(TreeNode<int>* root, vector<int> &ans) {
 
-    if(root -> left) {
-        traverseLeft(root -> left, ans);
-    }
-    else {
-        traverseLeft(root -> right, ans);
-    }
-}
-
-void traverseLeaf(TreeNode<int>* root, vector<int> &ans) {
-    //base case
-    if(root == NULL) {
-        return;
-    }
-
-    if(root -> left == NULL && root -> right == NULL) {
-        ans.push_back(root -> data);
-    }
-
-    if(root -> left) {
-        traverseLeaf(root -> left, ans);
-    }
-    if(root -> right) {
-        traverseLeaf(root -> right, ans);
-    }
-}
-
-void traverseRight(TreeNode<int>* root, vector<int> &ans) {
-    //base case
-    if((root == NULL) || (root -> left == NULL && root -> right == NULL)) {
-        return;
-    }
-
-    if(root -> right) {
-        traverseRight(root -> right, ans);
-    }
-    else {
-        traverseRight(root -> left, ans);
-    }
-    ans.push_back(root -> data);
-}
-vector<int> traverseBoundary(TreeNode<int>* root){
-    
-    vector<int> ans;
+        TreeNode<int>* curr = root;
         
-    if(root == NULL) {
+        while(curr != NULL) {
+            if(!isLeaf(curr)) {
+                ans.push_back(curr -> data);
+            }
+            if(curr -> left) {
+                curr = curr -> left;
+            }
+            else {
+                curr = curr -> right;
+            }
+        }
+    }
+
+    void traverseLeaf(TreeNode<int>* root, vector<int> &ans) {
+
+        if(isLeaf(root)) {
+            ans.push_back(root -> data);
+            return;
+        }
+
+        if(root -> left) {
+            traverseLeaf(root -> left, ans);
+        }
+        if(root -> right) {
+            traverseLeaf(root -> right, ans);
+        }
+    }
+
+    void traverseRight(TreeNode<int>* root, vector<int> &ans) {
+
+        TreeNode<int>* curr = root;
+        vector<int> temp;
+        
+        while(curr != NULL) {
+            if(!isLeaf(curr)) {
+                temp.push_back(curr -> data);
+            }
+            if(curr -> right) {
+                curr = curr -> right;
+            }
+            else {
+                curr = curr -> left;
+            }
+        }
+        
+        for(int i = temp.size()-1; i >= 0; i--) {
+            ans.push_back(temp[i]);
+        }
+    }
+
+    vector<int> traverseBoundary(TreeNode<int>* root){
+
+        vector<int> ans;
+        if(root == NULL) {
+            return ans;
+        }
+
+        if(!isLeaf(root)) {
+            ans.push_back(root -> data);
+        }
+
+        traverseLeft(root -> left, ans);
+        traverseLeaf(root, ans);
+        traverseRight(root -> right, ans);
+
         return ans;
     }
-
-    ans.push_back(root -> data);
-
-    //travserse left
-    traverseLeft(root -> left, ans);
-
-    //traverse leaf
-    //left part of root
-    traverseLeaf(root -> left, ans);
-
-    //right part of root
-    traverseLeaf(root -> right, ans);
-
-    //traverse right
-    traverseRight(root -> right, ans);
-
-    return ans;
-}
+};
