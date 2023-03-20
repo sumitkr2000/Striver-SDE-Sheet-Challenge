cd96855b7ee8d8e8a9c372a1f573c035bfcad15f
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:39:31 +0530

    Update Job Sequencing Problem.cpp

--- Day 8/Job Sequencing Problem.cpp
+++ Day 8/Job Sequencing Problem.cpp
@@ -1,33 +1,32 @@
-class Solution 
-{
-    private:
-    static bool comp(Job a, Job b) {
+class Solution  {
+private:
+    bool static comp(Job a, Job b) {
         return a.profit > b.profit;
     }
     
-    public:
-    vector<int> JobScheduling(Job arr[], int n) 
-    { 
-        sort(arr, arr + n, comp);
+public:
+    vector<int> JobScheduling(Job arr[], int n) { 
         
-        int maxDead = 0;
+        sort(arr, arr+n, comp);
+        
+        int maxi = INT_MIN;
         for(int i = 0; i < n; i++) {
-            maxDead = max(maxDead, arr[i].dead);
+            maxi = max(maxi, arr[i].dead);
         }
         
-        vector<int> jobSeq(maxDead+1, -1);
-        int jobs = 0, maxProfit = 0;
+        vector<int> jobSeq(maxi+1, -1);
+        int job = 0, profit = 0;
         
         for(int i = 0; i < n; i++) {
             for(int j = arr[i].dead; j > 0; j--) {
                 if(jobSeq[j] == -1) {
                     jobSeq[j] = arr[i].id;
-                    jobs++;
-                    maxProfit += arr[i].profit;
+                    job++;
+                    profit += arr[i].profit;
                     break;
                 }
             }
         }
-        return {jobs, maxProfit};
+        return {job, profit};
     } 
 };
