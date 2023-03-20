ade2cd943b41c406c933fc24279a669af4d12838
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:13:21 +0530

    Update Merge Intervals.cpp

--- Day 2/Merge Intervals.cpp
+++ Day 2/Merge Intervals.cpp
@@ -1,52 +1,27 @@
-/*
-
-    intervals[i][0] = start point of i'th interval
-    intervals[i][1] = finish point of i'th interval
-
-*/
-#include <bits/stdc++.h> 
-vector<vector<int>> mergeIntervals(vector<vector<int>> &intervals)
-{
-    sort(intervals.begin(), intervals.end());
-    int n = intervals.size();
-    vector<vector<int>> ans;
-    vector<int> temp;
-    int maxi = INT_MIN;
-
-    if(n == 1) {
-        return intervals;
-    }
-
-    for(int i = 0; i < n-1; i++) {
-
-        maxi = max(maxi, intervals[i][1]);
-
-        if(temp.empty()) {
-            temp.push_back(intervals[i][0]);
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        
+        if(n == 0) {
+            return ans;
         }
-
-        if(i == n-2 && maxi >= intervals[i+1][0]) {
-            temp.push_back(max(intervals[i+1][1],maxi));
-            ans.push_back(temp);
+        
+        vector<int> temp = intervals[0];
+        
+        for(int i = 0; i < n; i++) {
+            if(intervals[i][0] <= temp[1]) {
+                temp[1] = max(temp[1], intervals[i][1]);
+            }
+            else {
+                ans.push_back(temp);
+                temp = intervals[i];
+            }
         }
-
-        if(maxi < intervals[i+1][0]) {
-            temp.push_back(maxi);
-            ans.push_back(temp);
-            maxi = INT_MIN;
-            temp.clear();
-        }
-
-        if(i == n-2 && maxi < intervals[i+1][0]) {
-            ans.push_back(intervals[n-1]);
-        }
-    }
-
-    if(temp.size() == 1) {
-        temp.push_back(maxi);
         ans.push_back(temp);
+        return ans;
     }
-
-    return ans;
-}
-
+};
