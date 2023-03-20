a0ce14409f54909c5196e47b2f3b2836222dcdf1
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:30:35 +0530

    Update Diameter Of Binary Tree.cpp

--- Day 18/Diameter Of Binary Tree.cpp
+++ Day 18/Diameter Of Binary Tree.cpp
@@ -1,26 +1,24 @@
-pair<int,int> solve(TreeNode<int>* root) {
-      
-    if(root == NULL) {
-        pair<int,int> p = make_pair(0,0);
-        return p;
+class Solution {
+private:
+    int solve(TreeNode* root, int &ans) {
+        
+        if(root == NULL) {
+            return 0;
+        }
+
+        int left = solve(root -> left, ans);
+        int right = solve(root -> right, ans);
+        
+        ans = max(ans, left + right);
+
+        return 1 + max(left, right);
     }
-
-    pair<int, int> left = solve(root -> left);
-    pair<int, int> right = solve(root -> right);
-
-    int op1 = left.first;
-    int op2 = right.first;
-    int op3 = left.second + right.second + 1;
-
-    pair<int,int> ans;
-    ans.first = max(max(op1, op2), op3);
-    ans.second = max(left.second, right.second) + 1;
-
-    return ans;
-}
-
-int diameterOfBinaryTree(TreeNode<int> *root)
-{
-	return solve(root).first-1;
-}
-
+    
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        
+        int ans = 0;
+        solve(root, ans);
+        return ans;
+    }
+};
