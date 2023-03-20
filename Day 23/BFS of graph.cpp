9cb8a3f5a4122ee7b7142a029d1af17d109a66fd
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 15:02:13 +0530

    Update BFS of graph.cpp

--- Day 23/BFS of graph.cpp
+++ Day 23/BFS of graph.cpp
@@ -1,11 +1,11 @@
 class Solution {
-  public:
-    // Function to return Breadth First Traversal of given graph.
+public:
     vector<int> bfsOfGraph(int V, vector<int> adj[]) {
         
         vector<int> ans;
-        unordered_map<int, bool> vis;
+        vector<bool> vis(V, 0);
         queue<int> q;
+      
         q.push(0);
         vis[0] = 1;
         
