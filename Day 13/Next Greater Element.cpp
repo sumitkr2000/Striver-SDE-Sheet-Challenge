919a61d283e335fb2da104af09cfe830f0d223db
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 12:17:20 +0530

    Update Next Greater Element.cpp

--- Day 13/Next Greater Element.cpp
+++ Day 13/Next Greater Element.cpp
@@ -1,19 +1,18 @@
-#include<stack>
-vector<int> nextGreater(vector<int> &arr, int n) {
-    
-	stack<int> s;
-	s.push(-1);
-	vector<int> ans(n);
-	
-	for(int i = n-1; i >= 0; i--) {
-		int curr = arr[i];
-		
-		while(s.top() != -1 && s.top() <= curr) {
-			s.pop();
-		}
-		ans[i] = s.top();
-		s.push(curr);
-	}
-	return ans;
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        
+        int n = nums.size();
+        stack<int> st;
+        vector<int> ans(n, -1);
 
-}
+        for(int i = 0; i < n; i++) {
+	    while(!st.empty() && nums[i] > nums[st.top()]) {
+		ans[st.top()] = nums[i];
+		st.pop();
+	    }
+            st.push(i);
+        }
+        return ans;
+    }
+};
