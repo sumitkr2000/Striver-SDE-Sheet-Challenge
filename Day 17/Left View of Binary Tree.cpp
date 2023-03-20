dcd0578b63d9e50090c2f2b049e3e3a172a2fc37
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:13:45 +0530

    Update Left View of Binary Tree.cpp

--- Day 17/Left View of Binary Tree.cpp
+++ Day 17/Left View of Binary Tree.cpp
@@ -1,23 +1,27 @@
-void solve(Node* root, int lvl, vector<int> &ans) {
-    //base case
-    if(root == NULL) {
-        return;
-    }
-    
-    if(lvl == ans.size()) {
-        ans.push_back(root -> data);
-    }
-    
-    solve(root -> left, lvl+1, ans);
-    solve(root -> right, lvl+1, ans);
-}
+class Solution {
+private:
+    void solve(Node* root, int lvl, vector<int> &ans) {
 
-vector<int> leftView(Node *root)
-{
-   int lvl = 0;
-   vector<int> ans;
-   
-   solve(root, lvl, ans);
-   
-   return ans;
-}
+        if(root == NULL) {
+            return;
+        }
+
+        if(lvl == ans.size()) {
+            ans.push_back(root -> data);
+        }
+
+        solve(root -> left, lvl+1, ans);
+        solve(root -> right, lvl+1, ans);
+    }
+
+public:
+    vector<int> leftView(Node *root) {
+
+       int lvl = 0;
+       vector<int> ans;
+
+       solve(root, lvl, ans);
+
+       return ans;
+    }
+};
