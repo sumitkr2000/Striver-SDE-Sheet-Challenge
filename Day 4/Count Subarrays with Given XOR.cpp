79bc0d7c41ab7f615d83030764aea8212deb1e05
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:21:20 +0530

    Update Count Subarrays with Given XOR.cpp

--- Day 4/Count Subarrays with Given XOR.cpp
+++ Day 4/Count Subarrays with Given XOR.cpp
@@ -1,20 +1,21 @@
-#include <bits/stdc++.h> 
-int subarraysXor(vector<int> &arr, int x)
-{
-    unordered_map<int, int> mp;
-    int cnt = 0;
-    int xorr = 0;
-    
-    for(auto it: arr) {
-        xorr = xorr^it;
-        if(xorr == x) {
-            cnt++;
-        }
+class Solution {
+public:
+    int subarraysXor(vector<int> &arr, int x) {
         
-        if(mp.find(xorr^x) != mp.end()) {
-            cnt += mp[xorr^x];
+        unordered_map<int, int> mp;
+        int cnt = 0, xorr = 0;
+
+        for(auto it: arr) {
+            xorr = xorr^it;
+            if(xorr == x) {
+                cnt++;
+            }
+
+            if(mp.find(xorr^x) != mp.end()) {
+                cnt += mp[xorr^x];
+            }
+            mp[xorr]++;
         }
-        mp[xorr]++;
+        return cnt;
     }
-    return cnt;
-}
+};
