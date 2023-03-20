1cdc7051d2f5be8bf13f34390988c543d8edee79
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:27:19 +0530

    Update Delete Kth node From End.cpp

--- Day 5/Delete Kth node From End.cpp
+++ Day 5/Delete Kth node From End.cpp
@@ -1,24 +1,25 @@
-#include <bits/stdc++.h> 
-LinkedListNode<int>* removeKthNode(LinkedListNode<int> *head, int K)
-{
-   LinkedListNode<int>* dummy = new LinkedListNode<int>(1);
-   LinkedListNode<int>* fast = dummy;
-   LinkedListNode<int>* slow = dummy;
-
-   dummy -> next = head;
-
-   if(head == NULL || K == 0) {
-       return head;
-   }
-
-   for(int i = 1; i <= K; i++) {
-       fast = fast -> next;
-   }
-
-   while(fast -> next != NULL) {
-       fast = fast -> next;
-       slow = slow -> next;
-   }
-   slow -> next = slow -> next -> next;
-   return dummy -> next;
-}
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        ListNode* temp = new ListNode(0);
+        temp -> next = head;
+        ListNode *fast = temp, *slow = temp;
+        
+        while(n > 0) {
+            fast = fast -> next;
+            n--;
+        }
+        
+        if(fast -> next == NULL) {
+            return head -> next;
+        }
+        
+        while(fast -> next != NULL) {
+            fast = fast -> next;
+            slow = slow -> next;
+        }
+        slow -> next = slow -> next -> next;
+        return head;
+    }
+};
