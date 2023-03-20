22d646cad41caa3ca5522a1fdd21f6c9736596b2
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:58:48 +0530

    Update Nth Root Of M.cpp

--- Day 11/Nth Root Of M.cpp
+++ Day 11/Nth Root Of M.cpp
@@ -1,27 +1,29 @@
-double multiply(double m, int n) {
-    
-    double prod = 1.0;
-    for(int i = 1; i <= n; i++) {
-        prod *= m;
-    }
-    return prod;
-}
+class Solution {
+private:
+	double multiply(double m, int n) {
 
-double findNthRootOfM(int n, int m) {
-	
-    double s = 1;
-    double e = m;
-    double eps = 1e-9;
-    
-    while(e - s > eps) {
-        double mid = s + (e - s)/2.0;
-        
-        if(multiply(mid, n) < m) {
-            s = mid;
-        }
-        else {
-            e = mid;
-        }
-    }
-    return s;
-}
+	    double prod = 1.0;
+	    for(int i = 1; i <= n; i++) {
+		prod *= m;
+	    }
+	    return prod;
+	}
+
+public:
+	double findNthRootOfM(int n, int m) {
+
+	    double s = 1, e = m, eps = 1e-9;
+
+	    while(e - s > eps) {
+		double mid = s + (e - s)/2.0;
+
+		if(multiply(mid, n) < m) {
+		    s = mid;
+		}
+		else {
+		    e = mid;
+		}
+	    }
+	    return s;
+	}
+};
