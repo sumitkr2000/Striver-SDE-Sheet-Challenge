524bbdea1ad6f825af62b47fc3e2f92947769385
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 13:09:49 +0530

    Update Count and Say.cpp

--- Day 16/Count and Say.cpp
+++ Day 16/Count and Say.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     string countAndSay(int n) {
         
-        if(n == 1) {
-            return "1";
-        }
-        
         string ans = "1";
         
         for(int i = 2; i <= n; i++) {
