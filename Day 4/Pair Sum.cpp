50545880d6537ff1f98c0ff2a6d3324c0d038784
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 11:23:23 +0530

    Update Pair Sum.cpp

--- Day 4/Pair Sum.cpp
+++ Day 4/Pair Sum.cpp
@@ -1,19 +1,15 @@
-vector<vector<int>> pairSum(vector<int> &arr, int s){
-   vector<vector<int>> ans;
-    
-    for(int i = 0; i<arr.size(); i++) {
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
         
-        for(int j = i+1; j<arr.size(); j++) {
-            
-            if(arr[i] + arr[j] == s) {
-                
-                vector<int> temp;
-                temp.push_back(min(arr[i], arr[j]));
-                temp.push_back(max(arr[i], arr[j]));
-                ans.push_back(temp);
+        unordered_map<int, int> mp;
+       
+        for(int i = 0; i < nums.size(); i++) {
+            if(mp.find(target-nums[i]) != mp.end()) {
+                return {i, mp[target-nums[i]]};
             }
+            mp[nums[i]] = i;
         }
+        return {};
     }
-    sort(ans.begin(), ans.end());
-    return ans;
-}
+};
