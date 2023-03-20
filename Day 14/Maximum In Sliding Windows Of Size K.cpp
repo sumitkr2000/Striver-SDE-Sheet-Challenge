c0d6f66bd7ec0aa017f8816ba7c4367be5836d74
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-20 12:23:35 +0530

    Update Maximum In Sliding Windows Of Size K.cpp

--- Day 14/Maximum In Sliding Windows Of Size K.cpp
+++ Day 14/Maximum In Sliding Windows Of Size K.cpp
@@ -1,33 +1,25 @@
-#include<queue>
-vector<int> slidingWindowMaximum(vector<int> &nums, int &k)
-{
-    deque<int> maxi(k);
-    vector<int> ans;
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
+        deque<int> dq(k);
+        vector<int> ans;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            if(!dq.empty() && i - dq.front() == k) {
+                dq.pop_front();
+            }
+            
+            while(!dq.empty() && nums[i] >= nums[dq.back()]) {
+                dq.pop_back();
+            }
 
-    for(int i = 0; i < k; i++) {
-
-        while(!maxi.empty() && nums[maxi.back()] <= nums[i]) {
-            maxi.pop_back();
+            dq.push_back(i);
+            
+            if(i >= k-1) {
+                ans.push_back(nums[dq.front()]);
+            }
         }
-        maxi.push_back(i);
+        return ans;
     }
-    ans.push_back(nums[maxi.front()]);
-
-    for(int i = k; i < nums.size(); i++) {
-
-        //removal
-        while(!maxi.empty() && i-maxi.front() >= k) {
-            maxi.pop_front();
-        }
-
-        //addition
-        while(!maxi.empty() && nums[maxi.back()] <= nums[i]) {
-            maxi.pop_back();
-        }
-
-        maxi.push_back(i);
-
-        ans.push_back(nums[maxi.front()]);
-    }
-    return ans;
-}
+};
